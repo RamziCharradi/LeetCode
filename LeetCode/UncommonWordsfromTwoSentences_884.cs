4851211ed710c49c18c83383be20e42938d3ea53
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-03-28 19:20:42 +0100

    Uncommon Words from Two Sentences Solution

---
+++ LeetCode/UncommonWordsfromTwoSentences_884.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace LeetCode
+{
+    public class UncommonWordsfromTwoSentences_884
+    {
+        public static string[] UncommonFromSentences(string A, string B)
+        {
+            var arr_A = A.Split(" ");
+            var arr_B = B.Split(" ");
+
+            var arr_AA = arr_A.GroupBy(x => x)
+                                    .Where(x => x.Count() == 1)
+                                    .Select(x => x.Key).ToArray();
+            var arr_BB = B.Split(" ").GroupBy(x => x)
+                                    .Where(x => x.Count() == 1)
+                                    .Select(x => x.Key).ToArray();
+
+            var arr_X = arr_AA.Concat(arr_BB);
+
+            return arr_X.Where(x => !arr_A.Any(y => y == x) || !arr_B.Any(y => y == x))
+                        .ToArray();
+        }
+    }
+}
---
+++ LeetCode_test/UncommonWordsfromTwoSentences_884_UnitTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class UncommonWordsfromTwoSentences_884_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void Medthod1()
+        {
+            var Input_A = "this apple is sweet";
+            var Input_B = "this apple is sour";
+            var expected = new string[] { "sweet", "sour" };
+            var actual = UncommonWordsfromTwoSentences_884.UncommonFromSentences(Input_A, Input_B);
+            //CollectionAssert.AreEqual(expected, actual);
+            Assert.AreEqual(expected.Length, actual.Length);
+            foreach (var str in actual)
+            {
+                Assert.IsTrue(expected.ToList().Contains(str));
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void Medthod2()
+        {
+            var Input_A = "apple apple";
+            var Input_B = "banana";
+            var expected = new string[] { "banana" };
+            var actual = UncommonWordsfromTwoSentences_884.UncommonFromSentences(Input_A, Input_B);
+            //CollectionAssert.AreEqual(expected, actual);
+            Assert.AreEqual(expected.Length, actual.Length);
+            foreach (var str in actual)
+            {
+                Assert.IsTrue(expected.ToList().Contains(str));
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void Medthod3()
+        {
+            var Input_A = "s z z z s";
+            var Input_B = "s z ejt";
+            var expected = new string[] { "ejt" };
+            var actual = UncommonWordsfromTwoSentences_884.UncommonFromSentences(Input_A, Input_B);
+            //CollectionAssert.AreEqual(expected, actual);
+            Assert.AreEqual(expected.Length, actual.Length);
+            foreach (var str in actual)
+            {
+                Assert.IsTrue(expected.ToList().Contains(str));
+            }
+        }
+
+    }
+}
