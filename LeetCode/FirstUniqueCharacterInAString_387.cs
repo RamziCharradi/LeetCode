316b0ec95c7a157f3fcf4220585c0680d92cc8fc
Ramzi Charradi <ramzi_charradi@yahoo.fr>
2020-03-28 01:18:53 +0100

    A journey of a thousand miles begins with a single step

---
+++ LeetCode/FirstUniqueCharacterInAString_387.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace LeetCode
+{
+    public class FirstUniqueCharacterInAString_387
+    {
+        public static int FirstUniqChar(string s)
+        {
+            return s.IndexOf(s.GroupBy(x => x)
+                    .Where(x => x.Count() == 1)
+                    .Select(x => x.Key)
+                    .FirstOrDefault());
+        }
+    }
+}
---
+++ LeetCode_test/FirstUniqueCharacterInAString_387_UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class FirstUniqueCharacterInAString_387_UnitTest
+    {
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = "LeetCode";
+            var expected = 0;
+            var actual = FirstUniqueCharacterInAString_387.FirstUniqChar(input);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = "loveleetcode";
+            var expected = 2;
+            var actual = FirstUniqueCharacterInAString_387.FirstUniqChar(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
