d2f8fd7fb64d020d0b9dcd23df38a5df51aa4408
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-03-28 02:22:51 +0100

    Minimum Absolute Difference Solution

---
+++ LeetCode/MinimumAbsoluteDifference_1200.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class MinimumAbsoluteDifference_1200
+    {
+        public static IList<IList<int>> MinimumAbsDifference(int[] arr)
+        {
+            IList<IList<int>> l = new List<IList<int>>();
+            Array.Sort(arr);
+
+            int min = int.MaxValue;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int diff = (arr[i + 1] > arr[i]) ? arr[i + 1] - arr[i] : 0;
+
+                if (diff < min)
+                {
+                    l.Clear();
+                    l.Add(new List<int>() { arr[i], arr[i + 1] });
+                    min = diff;
+                }
+                else if (diff == min)
+                {
+                    l.Add(new List<int>() { arr[i], arr[i + 1] });
+                }
+
+            }
+
+            return l;
+
+        }
+    }
+}
---
+++ LeetCode_test/MinimumAbsoluteDifference_1200_UnitTest.cs
@@ -0,0 +1,71 @@
+﻿using LeetCode;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class MinimumAbsoluteDifference_1200_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] {4, 2, 1, 3};
+            IList<IList<int>> expected = new List<IList<int>>()
+            {   new List<int>() { 1, 2 },
+                new List<int>() { 2, 3 }, 
+                new List<int>() { 3, 4 } 
+            };
+            var actual = MinimumAbsoluteDifference_1200.MinimumAbsDifference(input);
+            Assert.AreEqual(expected.Count, actual.Count);
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i].Count, actual[i].Count);
+                for (var j = 0; j < expected[i].Count; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var input = new int[] { 1, 3, 6, 10, 15 };
+            IList<IList<int>> expected = new List<IList<int>>(){ new List<int>() { 1, 3 } };
+            var actual = MinimumAbsoluteDifference_1200.MinimumAbsDifference(input);
+            Assert.AreEqual(expected.Count, actual.Count);
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i].Count, actual[i].Count);
+                for (var j = 0; j < expected[i].Count; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var input = new int[] { 3, 8, -10, 23, 19, -4, -14, 27 };
+            IList<IList<int>> expected = new List<IList<int>>()
+            {   new List<int>() { -14,-10 },
+                new List<int>() { 19,23 },
+                new List<int>() { 23,27 }
+            };
+            var actual = MinimumAbsoluteDifference_1200.MinimumAbsDifference(input);
+            Assert.AreEqual(expected.Count, actual.Count);
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i].Count, actual[i].Count);
+                for (var j = 0; j < expected[i].Count; j++)
+                {
+                    Assert.AreEqual(expected[i][j], actual[i][j]);
+                }
+            }
+        }
+
+    }
+}
