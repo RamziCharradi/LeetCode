aeae207b7abb9a2bea7a1f3a38067b5da5a4ed58
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-03-29 00:50:19 +0100

    Rotate String Solution

---
+++ LeetCode/RotateString_796.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class RotateString_796
+    {
+        public static bool RotateString(string A, string B)
+        {
+            return A.Length == B.Length && (A+A).Contains(B);
+        }
+    }
+}
---
+++ LeetCode_test/RotateString_796_UnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class RotateString_796_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input_A = "abcde";
+            var Input_B = "cdeab";
+            var Expected = true;
+            var Actual = RotateString_796.RotateString(Input_A, Input_B);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input_A = "abcde";
+            var Input_B = "abced";
+            var Expected = false;
+            var Actual = RotateString_796.RotateString(Input_A, Input_B);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
