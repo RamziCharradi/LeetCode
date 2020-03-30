073c3e1d3e791695e4d1318be880390cb449be80
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-03-30 16:02:01 +0200

    Word Pattern Solution

---
+++ LeetCode/WordPattern_290.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class WordPattern_290
+    {
+        public static bool WordPattern(string pattern, string str)
+        {
+            var P = "";
+            var S = "";
+            var Array_Str = str.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var Array_Pat = pattern.ToCharArray();
+
+            var D_str = Array_Str.GroupBy(x=>x).ToDictionary(x => x.Key, x => Array.IndexOf(Array_Str, x.Key));
+
+            for (int i = 0; i < Array_Pat.Length; i++)
+            {
+                if(Array.IndexOf(Array_Pat, Array_Pat[i]) != D_str[Array_Str[i]])
+                {
+                    return false;
+                }
+            }
+
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode_test/WordPattern_290_UnitTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class WordPattern_290_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input_Pattern = "abba";
+            var Input_Str = "dog cat cat dog";
+            var Expected = true;
+            var Actual = WordPattern_290.WordPattern(Input_Pattern, Input_Str);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input_Pattern = "abba";
+            var Input_Str = "dog cat cat fish";
+            var Expected = false;
+            var Actual = WordPattern_290.WordPattern(Input_Pattern, Input_Str);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var Input_Pattern = "aaaa";
+            var Input_Str = "dog cat cat dog";
+            var Expected = false;
+            var Actual = WordPattern_290.WordPattern(Input_Pattern, Input_Str);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var Input_Pattern = "abba";
+            var Input_Str = "dog dog dog dog";
+            var Expected = false;
+            var Actual = WordPattern_290.WordPattern(Input_Pattern, Input_Str);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod5()
+        {
+            var Input_Pattern = "e";
+            var Input_Str = "eukera";
+            var Expected = true;
+            var Actual = WordPattern_290.WordPattern(Input_Pattern, Input_Str);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
