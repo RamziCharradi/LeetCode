b10649d34aa72f3c2eaed93f7f22abc09ab9588a
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-03-31 13:19:27 +0200

    784. Letter Case Permutation Solution
    Easy recursion solution .

---
+++ LeetCode/LetterCasePermutation_784.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class LetterCasePermutation_784
+    {
+        public static IList<string> LetterCasePermutation(string S)
+        {
+            IList<string> Result = new List<string>();
+            LetterCasePermutationHelper(S, 0, string.Empty, Result);
+            return Result;
+
+        }
+
+        private static void LetterCasePermutationHelper(string S, int index, string curr, IList<string> Result)
+        {
+            if (curr.Length == S.Length)
+                Result.Add(curr);
+            else
+            {
+                if (Char.IsDigit(S[index]))
+                {
+                    LetterCasePermutationHelper(S, index + 1, curr + S[index], Result);
+                }
+                else
+                {
+                    LetterCasePermutationHelper(S, index + 1, curr + Char.ToLower(S[index]), Result);
+                    LetterCasePermutationHelper(S, index + 1, curr + Char.ToUpper(S[index]), Result);
+                }
+            }
+
+        }
+    }
+}
---
+++ LeetCode_test/LetterCasePermutation_784_UnitTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class LetterCasePermutation_784_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = "a1b2";
+            IList<string> Expected = new List<string>() { "a1b2", "a1B2", "A1b2", "A1B2" };
+            var Actual = LetterCasePermutation_784.LetterCasePermutation(Input);
+            Assert.AreEqual(Expected.Count, Actual.Count);
+            for (var i = 0; i < Expected.Count; i++)
+            {
+                Assert.AreEqual(Expected[i], Actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input = "3z4";
+            IList<string> Expected = new List<string>() { "3z4", "3Z4" };
+            var Actual = LetterCasePermutation_784.LetterCasePermutation(Input);
+            Assert.AreEqual(Expected.Count, Actual.Count);
+            for (var i = 0; i < Expected.Count; i++)
+            {
+                Assert.AreEqual(Expected[i], Actual[i]);
+            }
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var Input = "12345";
+            IList<string> Expected = new List<string>() { "12345" };
+            var Actual = LetterCasePermutation_784.LetterCasePermutation(Input);
+            Assert.AreEqual(Expected.Count, Actual.Count);
+            for (var i = 0; i < Expected.Count; i++)
+            {
+                Assert.AreEqual(Expected[i], Actual[i]);
+            }
+        }
+    }
+}
