930802aac9f0a2a7a24e0747877556058df3f704
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-03-31 20:20:38 +0200

    1103. Distribute Candies to People Solution

---
+++ LeetCode/DistributeCandiesToPeople_1103.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class DistributeCandiesToPeople_1103
+    {
+        public static int[] DistributeCandies(int candies, int num_people)
+        {
+            var Final = new int[num_people];
+            var counter = 1;
+            var index = 0;
+            while (counter<candies)
+            {
+                //if (index == num_people) index = 0;
+                Final[index] += counter;
+                candies -= counter;
+                counter++;
+                index = (index + 1) % num_people;
+            }
+            //if (index == num_people) index = 0;
+            Final[index] += candies;
+            return Final;
+        }
+    }
+}
---
+++ LeetCode_test/DistributeCandiesToPeople_1103_UnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class DistributeCandiesToPeople_1103_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input_Candies = 7;
+            var Input_num_people = 4;
+            var Expected = new int[]{ 1, 2, 3, 1 };
+            var Actual = DistributeCandiesToPeople_1103.DistributeCandies(Input_Candies, Input_num_people);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input_Candies = 10;
+            var Input_num_people = 3;
+            var Expected = new int[] { 5, 2, 3 };
+            var Actual = DistributeCandiesToPeople_1103.DistributeCandies(Input_Candies, Input_num_people);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+    }
+}
