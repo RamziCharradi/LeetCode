f5ca03c0accd4ccaf4d5e275e941de834440788b
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-03-31 22:38:26 +0200

    944. Delete Columns to Make Sorted

---
+++ LeetCode/DeleteColumnsToMakeSorted_944.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class DeleteColumnsToMakeSorted_944
+    {
+        public static int MinDeletionSize(string[] A)
+        {
+            var Result = 0;
+            for (int i = 0; i <A[0].Length ; i++)
+            {
+                bool test = false;
+                var prev = A[0][i];
+                for (int j = 0; j < A.Length; j++)
+                {
+                    if( prev> A[j][i])
+                    {
+                        test = true;
+                        break;
+
+                    }
+                    prev = A[j][i];
+                }
+                if (test) Result++; 
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ LeetCode_test/DeleteColumnsToMakeSorted_944_UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class DeleteColumnsToMakeSorted_944_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new string[] { "cba", "daf", "ghi" };
+            var Expected = 1;
+            var Actual = DeleteColumnsToMakeSorted_944.MinDeletionSize(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input = new string[] { "a", "b" };
+            var Expected = 0;
+            var Actual = DeleteColumnsToMakeSorted_944.MinDeletionSize(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var Input = new string[] { "zyx", "wvu", "tsr" };
+            var Expected = 3;
+            var Actual = DeleteColumnsToMakeSorted_944.MinDeletionSize(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
