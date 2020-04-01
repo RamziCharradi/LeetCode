9d52af4bb8bb9172a00b29165d8a5ef78afcf315
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-01 03:32:37 +0200

    844. Backspace String Compare

---
+++ LeetCode/BackspaceStringCompare_844.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class BackspaceStringCompare_844
+    {
+        public static bool BackspaceCompare(string S, string T)
+        {
+            Stack<char> Ss = new Stack<char>();
+            foreach (var c in S)
+            {
+                if (!char.IsLetter(c))
+                {
+                    if (Ss.Count > 0) Ss.Pop();
+                }
+                else
+                {
+                    Ss.Push(c);
+                }
+            }
+            Stack<char> St = new Stack<char>();
+            foreach (var c in T)
+            {
+                if (!char.IsLetter(c))
+                {
+                    if (St.Count > 0) St.Pop();
+                }
+                else
+                {
+                    St.Push(c);
+                }
+            }
+
+            return new String(Ss.ToArray()) == new String(St.ToArray());
+        }
+
+        //public static Stack<char> Helper(String input)
+        //{
+        //    Stack<char> S = new Stack<char>();
+
+        //    foreach (var c in input)
+        //    {
+        //        if (!char.IsLetter(c))
+        //        {
+        //            if (S.Count > 0) S.Pop();
+        //        }
+        //        else
+        //        {
+        //            S.Push(c);
+        //        }
+        //    }
+        //    return S;
+        //}
+    }
+}
---
+++ LeetCode_test/BackspaceStringCompare_844_UnitTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class BackspaceStringCompare_844_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input_S = "ab#c";
+            var Input_T = "ad#c";
+            var Expected = true;
+            var Actual = BackspaceStringCompare_844.BackspaceCompare(Input_S, Input_T);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input_S = "ab##";
+            var Input_T = "c#d#";
+            var Expected = true;
+            var Actual = BackspaceStringCompare_844.BackspaceCompare(Input_S, Input_T);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var Input_S = "a##c";
+            var Input_T = "#a#c";
+            var Expected = true;
+            var Actual = BackspaceStringCompare_844.BackspaceCompare(Input_S, Input_T);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var Input_S = "a#c";
+            var Input_T = "b";
+            var Expected = false;
+            var Actual = BackspaceStringCompare_844.BackspaceCompare(Input_S, Input_T);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod5()
+        {
+            var Input_S = "xywrrmp";
+            var Input_T = "xywrrmu#p";
+            var Expected = true;
+            var Actual = BackspaceStringCompare_844.BackspaceCompare(Input_S, Input_T);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
