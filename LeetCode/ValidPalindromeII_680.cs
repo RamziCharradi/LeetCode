b6ed62dc9b23ab6b84f4a0a9f50246690fd4608e
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-02 01:58:28 +0200

    680. Valid Palindrome II Solution

---
+++ LeetCode/ValidPalindromeII_680.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ValidPalindromeII_680
+    {
+        public static bool ValidPalindrome(string s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            while (i < j)
+            {
+                if (s[i] != s[j])
+                    break;
+                i++; j--;
+            }
+            if (i >= j) return true;
+            return (IsPalindrome(s.Substring(i, j - i)) || IsPalindrome(s.Substring(i + 1, j - i)));
+        }
+        public static bool IsPalindrome(string s)
+        {
+            if (s.Length == 0) return true;
+            int i = 0, j = s.Length - 1;
+                while (i < j)
+                {
+                    if (s[i] != s[j])
+                        break;
+                    i++; j--;
+                }
+            if (i >= j) return true;
+            return false;
+        }
+    }
+
+}
