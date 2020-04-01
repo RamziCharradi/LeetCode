5efc96d838c0457ac2e65d3e1f6a6ed9fd6f95d7
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-01 14:51:32 +0200

    929. Unique Email Addresses

---
+++ LeetCode/UniqueEmailAddresses_929.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class UniqueEmailAddresses_929
+    {
+        public static int NumUniqueEmails(string[] emails)
+        {
+            return emails.Select(x => x.Split('@'))
+                         .Select(x => x[0].Split('+')[0].Replace(".", "") + "@" + x[1])
+                         .Distinct()
+                         .Count();
+        }
+    }
+}
---
+++ LeetCode_test/UniqueEmailAddresses_929_UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class UniqueEmailAddresses_929_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new string[] { @"test.email+alex@leetcode.com", @"test.e.mail+bob.cathy@leetcode.com", @"testemail+david@lee.tcode.com" };
+            var Expected = 2;
+            var Actual = UniqueEmailAddresses_929.NumUniqueEmails(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
