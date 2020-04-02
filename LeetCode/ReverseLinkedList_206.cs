f21b91a4c4d8ce115763ff52857465bb129ef1df
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-02 16:43:10 +0200

    206. Reverse Linked List

---
+++ LeetCode/ReverseLinkedList_206.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ReverseLinkedList_206
+    {
+        public static ListNode ReverseList(ListNode head)
+        {
+            List<int> track = new List<int>();
+            Helper(head,track);
+            if (track.Count > 0)
+            {
+                int i = track.Count - 1;
+                ListNode ReversedLinkedList = new ListNode(track[i--]);
+                LinkedList(ref ReversedLinkedList.next, i,track);
+                return ReversedLinkedList;
+            }
+            return null;
+            
+        }
+
+        public static void Helper(ListNode node, List<int> l)
+        {
+            if(node!=null)
+            {
+                l.Add(node.val);
+                Helper(node.next, l);
+            }
+            return;
+        }
+
+        public static void LinkedList(ref ListNode head,int counter, List<int> l)
+        {
+            if (counter>=0)
+            {
+                head = new ListNode(l[counter--]);
+                if (counter >= 0) LinkedList(ref head.next, counter, l);
+            }
+            return;
+        }
+    }
+}
---
+++ LeetCode_test/ReverseLinkedList_206_UnitTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class ReverseLinkedList_206_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                        }
+                    }
+                }
+            };
+            var Expected = new ListNode(5)
+            {
+                next = new ListNode(4)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(2)
+                        {
+                            next = new ListNode(1)
+                        }
+                    }
+                }
+            };
+
+            var Actual = ReverseLinkedList_206.ReverseList(Input);
+            
+            while (Expected.next != null && Actual.next != null)
+            {
+                Assert.AreEqual(Expected.val, Actual.val);
+                Expected = Expected.next;
+                Actual = Actual.next;
+            }
+            
+            
+        }
+
+    }
+}
