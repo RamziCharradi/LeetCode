062558c13126f53e5d6c412abb47c1066872df18
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-03 00:06:19 +0200

    169. Majority Element
    One Line Solution

---
+++ LeetCode/MajorityElement_169.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class MajorityElement_169
+    {
+        public static int MajorityElement(int[] nums) 
+        {
+            /*
+             return  nums.GroupBy(x => x).Where(x => x.Count() > nums.Length / 2)
+                         .Select(x => x.Key).FirstOrDefault();
+             */
+            return nums.GroupBy(x => x).Where(x => x.Count() > nums.Length / 2)
+                        .First().Key;
+        } 
+    }
+}
---
+++ LeetCode_test/MajorityElement_169_UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class MajorityElement_169_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMedthod1()
+        {
+            var Input = new int[] {3, 2, 3 };
+            var Expected = 3;
+            var Actual = MajorityElement_169.MajorityElement(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        
+        [TestMethod, Timeout(20_000)]
+        public void TestMedthod2()
+        {
+            var Input = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+            var Expected = 2;
+            var Actual = MajorityElement_169.MajorityElement(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
