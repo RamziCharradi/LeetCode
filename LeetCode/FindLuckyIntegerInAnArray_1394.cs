b974da29bd288ba5b52555eed23f63a8ca93eead
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-02 23:35:55 +0200

    1394. Find Lucky Integer in an Array Solution
    One line Solution

---
+++ LeetCode/FindLuckyIntegerInAnArray_1394.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class FindLuckyIntegerInAnArray_1394
+    {
+        public static int FindLucky(int[] arr) => arr.GroupBy(x => x).Where(x => x.Count() == x.Key)
+                                      .Select(x => x.Key).OrderByDescending(x=>x)
+                                      .DefaultIfEmpty(-1).First();
+        
+    }
+}
---
+++ LeetCode_test/FindLuckyIntegerInAnArray_1394_UnitTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class FindLuckyIntegerInAnArray_1394_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMedthod1()
+        {
+            var Input = new int[] { 2, 2, 3, 4 };
+            var Expected = 2;
+            var Actual = FindLuckyIntegerInAnArray_1394.FindLucky(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMedthod2()
+        {
+            var Input = new int[] { 1, 2, 2, 3, 3, 3 };
+            var Expected = 3;
+            var Actual = FindLuckyIntegerInAnArray_1394.FindLucky(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMedthod3()
+        {
+            var Input = new int[] { 2, 2, 2, 3, 3 };
+            var Expected = -1;
+            var Actual = FindLuckyIntegerInAnArray_1394.FindLucky(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMedthod4()
+        {
+            var Input = new int[] { 5 };
+            var Expected = -1;
+            var Actual = FindLuckyIntegerInAnArray_1394.FindLucky(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMedthod5()
+        {
+            var Input = new int[] { 7, 7, 7, 7, 7, 7, 7 };
+            var Expected = 7;
+            var Actual = FindLuckyIntegerInAnArray_1394.FindLucky(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
