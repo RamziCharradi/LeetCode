28bbecd4d700ef370b46b223a20a60713fea07f5
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-04 00:26:46 +0200

    905. Sort Array By Parity Solution

---
+++ LeetCode/SortArrayByParity_905.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class SortArrayByParity_905
+    {
+        public static int[] SortArrayByParity(int[] A)
+        {
+            return A.Where(x => x % 2 == 0)
+                    .Concat(A.Where(x => x % 2 == 1))
+                    .ToArray();
+            
+        }
+       
+    }
+}
---
+++ LeetCode_test/SortArrayByParity_905_UnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class SortArrayByParity_905_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var input = new int[] { 3, 1, 2, 4 };
+            var expected1 = new int[] { 2, 4, 3, 1 };
+            var expected2 = new int[] { 4, 2, 3, 1 };
+            var expected3 = new int[] { 2, 4, 1, 3 };
+            var expected4 = new int[] { 4, 2, 1, 3 };
+
+            var Actual = SortArrayByParity_905.SortArrayByParity(input);
+            Assert.IsTrue(Enumerable.SequenceEqual(expected1,Actual)|| 
+                          Enumerable.SequenceEqual(expected2, Actual)||
+                          Enumerable.SequenceEqual(expected3, Actual)||
+                          Enumerable.SequenceEqual(expected4, Actual));
+        }
+    }
+}
