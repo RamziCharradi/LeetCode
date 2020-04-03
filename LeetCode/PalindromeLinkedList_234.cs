cdb80edc459d3e6f8bbff6a96554ce9bcc2417a4
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-03 02:38:42 +0200

    234. Palindrome Linked List

---
+++ LeetCode/PalindromeLinkedList_234.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class PalindromeLinkedList_234
+    {
+        public static bool IsPalindrome(ListNode head)
+        {
+            List<object> l = new List<object>() {};
+            Stack<object> stk = new Stack<object>();
+            while (head != null)
+            {
+                stk.Push(head.val);
+                l.Add(head.val);
+                head = head.next;
+            }
+
+            return l.SequenceEqual(stk.ToList());
+            //return l.SequenceEqual(l.Select(x => x).Reverse());
+        }
+    }
+}
---
+++ LeetCode_test/PalindromeLinkedList_234_UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class PalindromeLinkedList_234_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new ListNode(1) { next = new ListNode(2) };
+            var Expected = false;
+            var Actual = PalindromeLinkedList_234.IsPalindrome(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input = new ListNode(1) { next = new ListNode(2) {next = new ListNode(2) {next = new ListNode(1) } } };
+            var Expected = true;
+            var Actual = PalindromeLinkedList_234.IsPalindrome(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var Input = new ListNode(-121) { next = new ListNode(-121) };
+            var Expected = true;
+            var Actual = PalindromeLinkedList_234.IsPalindrome(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
