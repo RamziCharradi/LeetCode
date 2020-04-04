8c3c3e8cfc9db67bdca119add1bfced136d3ac86
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-04 21:27:11 +0200

    922. Sort Array By Parity II

---
+++ LeetCode/SortArrayByParityII_922.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class SortArrayByParityII_922
+    {
+        public static int[] SortArrayByParityII(int[] A)
+        {
+            int odd = 0;
+            int even = 1;
+            int[] Result = new int[A.Length];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i]% 2 == 0)
+                {
+                    Result[odd] = A[i];
+                    odd += 2;
+                }
+                else
+                {
+                    Result[even] = A[i];
+                    even += 2;
+                }
+            }
+            return Result;
+        }
+    }
+}
---
+++ LeetCode_test/SortArrayByParityII_922_UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{ 
+    [TestClass]
+    public class SortArrayByParityII_922_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 4, 2, 5, 7 };
+            var Expected = new int[] { 4, 5, 2, 7 };
+            var Actual = SortArrayByParityII_922.SortArrayByParityII(Input);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
