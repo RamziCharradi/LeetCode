5f4ab0fdd22af9171d7837d17c95d5790cb0a692
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-05 20:48:09 +0200

    860. Lemonade Change Solution

---
+++ LeetCode/LemonadeChange_860.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class LemonadeChange_860
+    {
+        public static bool LemonadeChange(int[] bills)
+        {
+            Dictionary<int, int> d = new Dictionary<int, int> { { 5, 0 }, { 10, 0 } };
+            foreach (var b in bills)
+            {
+                if (b == 5) d[5]++;
+                
+                if (b == 10)
+                {
+                    if (d[5] == 0) return false;
+                    else
+                    {
+                        d[5]--;
+                        d[10]++;
+                    }
+                }
+                if(b == 20)
+                {
+                    if (d[10] == 0)
+                    {
+                        if (d[5] < 3) return false;
+                        else
+                        {
+                            d[5]-=3;
+                        }
+                    }
+                    else
+                    {
+                        if (d[5] == 0) return false;
+                        else
+                        {
+                            d[5]--;
+                            d[10]--;
+                        }
+                    }
+                }
+            }
+            return true;
+            
+        }
+    }
+}
---
+++ LeetCode_test/LemonadeChange_860_UnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class LemonadeChange_860_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 5, 5, 5, 10, 20 };
+            var Expected = true;
+            var Actual = LemonadeChange_860.LemonadeChange(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input = new int[] { 5, 5, 10 };
+            var Expected = true;
+            var Actual = LemonadeChange_860.LemonadeChange(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var Input = new int[] { 10 , 10 };
+            var Expected = false;
+            var Actual = LemonadeChange_860.LemonadeChange(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var Input = new int[] { 5, 5, 10, 10, 20 };
+            var Expected = false;
+            var Actual = LemonadeChange_860.LemonadeChange(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
