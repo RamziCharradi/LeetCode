f8c0612011e204ff9fe28c05a777080030673556
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-05 19:12:16 +0200

    1395. Count Number of Teams Solution

---
+++ LeetCode/CountNumberOfTeams_1395.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class CountNumberOfTeams_1395
+    {
+        public static int NumTeams(int[] rating)
+        {
+            int n = rating.Length; 
+            int count = 0;
+
+            for (int i = 1; i < n - 1; i++)
+            {
+                int curr = rating[i];
+                int leftSmaller = 0;
+                int leftBigger = 0;
+                int rightSmaller = 0;
+                int rightBigger = 0;
+                //2,5,3,4,1
+                for (int l = 0; l < i; l++)
+                {
+                    if (rating[l] < curr) leftSmaller++; //1
+                    if (rating[l] > curr) leftBigger++; //1
+                }
+
+                for (int r = i + 1; r < n; r++)
+                {
+                    if (rating[r] < curr) rightSmaller++; //1
+                    if (rating[r] > curr) rightBigger++; //1
+                }
+
+                count += (leftSmaller * rightBigger) + (leftBigger * rightSmaller);
+            }
+            return count;
+
+        }
+
+   
+    }
+}
---
+++ LeetCode_test/CountNumberOfTeams_1395_UnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class CountNumberOfTeams_1395_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 2, 5, 3, 4, 1 };
+            var Expected = 3;
+            var Actual = CountNumberOfTeams_1395.NumTeams(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input = new int[] { 2, 1, 3 };
+            var Expected = 0;
+            var Actual = CountNumberOfTeams_1395.NumTeams(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var Input = new int[] { 1, 2, 3, 4 };
+            var Expected = 4;
+            var Actual = CountNumberOfTeams_1395.NumTeams(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
