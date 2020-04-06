35536c9ee75e1e7a1938743f125bc0a1675a1a3d
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-06 15:44:56 +0200

    49. Group Anagrams Solution

---
+++ LeetCode/GroupAnagrams_49.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class GroupAnagrams_49
+    {
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+           // IList<IList<string>> Result = new List<IList<string>>();
+            Dictionary<string, IList<string>> d = new Dictionary<string, IList<string>>();
+            foreach (var s in strs)
+            {
+                string os = String.Concat(s.OrderBy(c => c))
+;
+                if (d.Keys.Contains(os))
+                {
+                    d[os].Add(s);
+                }
+                else
+                {
+                    d.Add(os, new List<string> { s });
+                }
+            }
+
+            //Result = d.Select(x => x.Value).ToList();
+            return d.Select(x => x.Value).ToList();
+        }
+    }
+}
---
+++ LeetCode_test/GroupAnagrams_49_UnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System.Linq;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class GroupAnagrams_49_UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var Input = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+            IList<IList<string>> Expected = new List<IList<string>>()
+            {
+                new List<string>{"ate","eat","tea"},
+                new List<string>{"nat","tan"},
+                new List<string>{ "bat"}
+            };
+            var Actual = GroupAnagrams_49.GroupAnagrams(Input);
+            Assert.AreEqual(Expected.Count, Actual.Count);
+            for (int i = 0; i < Expected.Count; i++)
+            {
+                for (int j = 0; j < Expected[i].Count; j++)
+                {
+                    CollectionAssert.Contains(Expected[i].ToList(), Actual[i][j]);
+                }
+            }
+        }
+    }
+}
