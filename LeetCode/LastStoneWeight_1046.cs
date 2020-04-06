79aa51c236a183d65e29a10275ac6ddc40af04e0
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-06 23:09:25 +0200

    1046. Last Stone Weight Solution

---
+++ LeetCode/LastStoneWeight_1046.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class LastStoneWeight_1046
+    {
+        public static int LastStoneWeight(int[] stones)
+        {
+            Array.Sort(stones);
+            List<int> l = stones.ToList();
+            int lastIdx = l.Count - 1;
+            int prevIdx = lastIdx - 1;
+
+            while (lastIdx > 0)
+            {
+                l[prevIdx] = l[lastIdx] - l[prevIdx];
+                l.RemoveAt(lastIdx);
+                l.Sort();
+                lastIdx--;
+                prevIdx--;
+            }
+
+            return l[lastIdx];
+
+
+            //Array.Sort(stones);
+            //List<int> l = stones.ToList();
+            //while (l.Count > 1)
+            //{
+            //    int Max1 = l[l.Count - 1];
+            //    int Max2 = l[l.Count - 2];
+            //    l.RemoveAt(l.Count - 1);
+            //    l.RemoveAt(l.Count - 1);
+
+            //    if (Max1 != Max2)
+            //    {
+            //        int liIndex = l.BinarySearch(Max1 - Max2);
+
+            //        if (liIndex < 0)
+            //            liIndex = ~liIndex;
+
+            //        l.Insert(liIndex, Max1 - Max2);
+            //    }
+            //}
+
+            //return l.Count == 0 ? 0 : l[0];
+        }
+    }
+}
---
+++ LeetCode_test/LastStoneWeight_1046_UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public  class LastStoneWeight_1046_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 2, 7, 4, 1, 8, 1 };
+            var Expected = 1;
+            var Actual = LastStoneWeight_1046.LastStoneWeight(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
