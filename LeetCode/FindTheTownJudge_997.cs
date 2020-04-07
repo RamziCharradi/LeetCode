6ebbfaee702210676f425cb86691f09b3391c840
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-07 15:13:20 +0200

    997. Find the Town Judge Solution

---
+++ LeetCode/FindTheTownJudge_997.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class FindTheTownJudge_997
+    {
+        public static int FindJudge(int N, int[][] trust)
+        {
+            List<int> TrustNobody = (List<int>)Enumerable.Range(1, N).ToList()
+                                    .Where(x => !trust.Any(y => y[0] == x)).ToList();
+                                        
+
+            foreach (var x in TrustNobody)
+            {
+                if(trust.Select(y => y).Select(y => y[1]).Where(y => y == x).Count() == N - 1)
+                {
+                    return x;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode_test/FindTheTownJudge_997_UnitTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class FindTheTownJudge_997_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input_N = 2;
+            var Input_Trust = new int[][] { new int[] { 1, 2 } };
+            var Expected = 2;
+            var Actual = FindTheTownJudge_997.FindJudge(Input_N, Input_Trust);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input_N = 3;
+            var Input_Trust = new int[][] { new int[] { 1, 3 } , new int[] { 2, 3 } };
+            var Expected = 3;
+            var Actual = FindTheTownJudge_997.FindJudge(Input_N, Input_Trust);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var Input_N = 3;
+            var Input_Trust = new int[][]
+            {
+                new int[] { 1, 3 },
+                new int[] { 2, 3 },
+                new int[] { 3, 1 }
+            };
+            var Expected = -1;
+            var Actual = FindTheTownJudge_997.FindJudge(Input_N, Input_Trust);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var Input_N = 3;
+            var Input_Trust = new int[][]
+            {
+                new int[] { 1, 2 },
+                new int[] { 2, 3 }
+            };
+            var Expected = -1;
+            var Actual = FindTheTownJudge_997.FindJudge(Input_N, Input_Trust);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod5()
+        {
+            var Input_N = 4;
+            var Input_Trust = new int[][]
+            {
+                new int[] { 1, 3 },
+                new int[] { 1, 4 },
+                new int[] { 2, 3 },
+                new int[] { 2, 4 },
+                new int[] { 4, 3 }
+            };
+            var Expected = 3;
+            var Actual = FindTheTownJudge_997.FindJudge(Input_N, Input_Trust);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
