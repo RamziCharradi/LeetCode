e7d0123764be66e807dfa4d516d68eaaa73d6002
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-07 17:26:13 +0200

    Count Elements Solution

---
+++ LeetCode/CountingElements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class CountingElements
+    {
+        public static int CountElements(int[] arr)
+        {
+            return arr.Select(x => x + 1).Where(x => arr.Any(y => y == x)).Count();
+
+        }
+    }
+}
---
+++ LeetCode_test/CountingElements_UnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class CountingElements_UnitTest
+    {
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 1, 2, 3 };
+            var Expected = 2;
+            var Actual = CountingElements.CountElements(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input = new int[] { 1, 1, 3, 3, 5, 5, 7, 7 };
+            var Expected = 0;
+            var Actual = CountingElements.CountElements(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var Input = new int[] { 1, 3, 2, 3, 5, 0 };
+            var Expected = 3;
+            var Actual = CountingElements.CountElements(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod4()
+        {
+            var Input = new int[] { 1, 1, 2, 2 };
+            var Expected = 2;
+            var Actual = CountingElements.CountElements(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
