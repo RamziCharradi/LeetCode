d28e3df7e4240d8f60cca70407389ef0526f3688
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-07 21:03:43 +0200

    914. X of a Kind in a Deck of Cards Solution

---
+++ LeetCode/XOfAKindInADeckOfCards_914.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class XOfAKindInADeckOfCards_914
+    {
+        public static bool HasGroupsSizeX(int[] deck)
+        {
+            Dictionary<int, int> d = deck.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+            if (d.ContainsValue(1))
+            {
+                return false;
+            }
+            else
+            {
+                
+                var gcd = d.Values.Min();
+                foreach (var x in d.Values.Distinct())
+                {
+                    gcd = GCD(x, gcd);
+
+                }
+
+                return gcd > 1;
+            }
+
+            
+        }
+
+        static int GCD(int a, int b)
+        {
+            int Remainder;
+
+            while (b != 0)
+            {
+                Remainder = a % b;
+                a = b;
+                b = Remainder;
+            }
+
+            return a;
+        }
+    }
+}
---
+++ LeetCode_test/XOfAKindInADeckOfCards_914_UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class XOfAKindInADeckOfCards_914_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        [DataRow(new int[] { 1, 2, 3, 4, 4, 3, 2, 1 },true)]
+        [DataRow(new int[] { 1, 1, 1, 2, 2, 2, 3, 3 }, false)]
+        [DataRow(new int[] { 1 }, false)]
+        [DataRow(new int[] { 1, 1 }, true)]
+        [DataRow(new int[] { 1, 1, 2, 2, 2, 2 }, true)]
+        [DataRow(new int[] { 1, 1, 1, 1, 2, 2, 2, 2, 2, 2 },true)]
+        public void TestMethod1(int[] Input, bool Expected)
+        {
+            var Actual = XOfAKindInADeckOfCards_914.HasGroupsSizeX(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
