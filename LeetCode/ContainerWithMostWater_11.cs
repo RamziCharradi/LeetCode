efe78f95a38699970c7717ce2c298c6e771a518e
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-08 11:08:42 +0200

    11. Container With Most Water Solution

---
+++ LeetCode/ContainerWithMostWater_11.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class ContainerWithMostWater_11
+    {
+        public static int MaxArea(int[] height)
+        {
+            var max = 0;
+            for (int i = 0; i < height.Length-1; i++)
+            {
+                for (int j = height.Length-1 ; j >i ; j--)
+                {
+                    max = Math.Max(max, (j - i) * (Math.Min(height[i], height[j])));
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCode_test/ContainerWithMostWater_11_UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class ContainerWithMostWater_11_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            var Expected = 49;
+            var Actual = ContainerWithMostWater_11.MaxArea(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input = new int[] { 1,2 };
+            var Expected = 1;
+            var Actual = ContainerWithMostWater_11.MaxArea(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod3()
+        {
+            var Input = new int[] { 2, 3, 4, 5, 18, 17, 6 };
+            var Expected = 17;
+            var Actual = ContainerWithMostWater_11.MaxArea(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
