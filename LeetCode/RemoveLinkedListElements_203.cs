86c3d379630e3b88d6c33cbbe5b73b40b6560f40
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-09 15:26:28 +0200

    203. Remove Linked List Elements

---
+++ LeetCode/RemoveLinkedListElements_203.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class RemoveLinkedListElements_203
+    {
+        public static ListNode RemoveElements(ListNode head, int val)
+        {
+            if (head == null)
+                return null;
+
+            if (head.val == val)
+                return RemoveElements(head.next, val);
+
+            head.next = RemoveElements(head.next, val);
+
+            return head;
+        }
+
+    }
+}
---
+++ LeetCode_test/RemoveLinkedListElements_203_UnitTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class RemoveLinkedListElements_203_UnitTest
+    {
+       [TestMethod]
+       public void TestMethod1()
+       {
+            var Input_head = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(6)
+                    {
+                        next = new ListNode(3)
+                        {
+                            next = new ListNode(4)
+                            {
+                                next = new ListNode(5)
+                                {
+                                    next = new ListNode(6)
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            var Input_val = 6;
+
+            var Expected = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                        }
+                    }
+                }
+            };
+
+            var Actual = RemoveLinkedListElements_203.RemoveElements(Input_head, Input_val);
+
+            while (Expected != null && Actual != null)
+            {
+                Assert.AreEqual(Expected.val, Actual.val);
+                Expected = Expected.next;
+                Actual = Actual.next;
+            }
+        }
+    }
+}
