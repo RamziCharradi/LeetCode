6dc46b22b5b4a452a773a4b3c2abaeff4b7986c3
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-09 10:35:46 +0200

    26. Remove Duplicates from Sorted Array

---
+++ LeetCode/RemoveDuplicatesFromSortedArray_26.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class RemoveDuplicatesFromSortedArray_26
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+            set.CopyTo(nums);
+            return set.Count();
+        }
+    }
+}
---
+++ LeetCode_test/RemoveDuplicatesFromSortedArray_26_UnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System.Linq;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class RemoveDuplicatesFromSortedArray_26_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 1, 1, 2 };
+            var Expected = 2;
+            var Actual = RemoveDuplicatesFromSortedArray_26.RemoveDuplicates(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            var Expected = 5;
+            var Actual = RemoveDuplicatesFromSortedArray_26.RemoveDuplicates(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
