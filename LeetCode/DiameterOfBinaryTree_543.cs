3f5eedd1ddbb7189e6e2c0392742e261e60f221b
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-11 20:30:02 +0200

    Diameter Of Binary Tree

---
+++ LeetCode/DiameterOfBinaryTree_543.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class DiameterOfBinaryTree_543
+    {
+        private static int Result;
+        public static int DiameterOfBinaryTree(TreeNode root)
+        {
+            Result = 0;
+            MaxDepth(root);
+            return Result;
+        }
+        public static int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            int left = MaxDepth(root.left);
+            int right = MaxDepth(root.right);
+            Result = Math.Max(left + right, Result);
+            return Math.Max(left, right) + 1;
+        }
+    }
+}
---
+++ LeetCode_test/DiameterOfBinaryTree_543_UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class DiameterOfBinaryTree_543_UnitTest
+    {
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(4),
+                    right = new TreeNode(5)
+                },
+                right = new TreeNode(3)
+            };
+
+            var Expected = 3;
+            var Actual = DiameterOfBinaryTree_543.DiameterOfBinaryTree(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
