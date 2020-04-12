e9c180108cfca4f5aa83ce529bb483ae9a776714
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-12 12:34:46 +0200

    House Robber
    DP

---
+++ LeetCode/HouseRobber_198.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class HouseRobber_198
+    {
+        public static int Rob(int[] nums)
+        {
+
+            int[] dp = new int[nums.Length + 1];
+            dp[0] = 0;
+            dp[1] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                dp[i+1] = Math.Max(dp[i],nums[i]+dp[i-1]);
+            }
+
+            return dp[nums.Length];
+        }
+    }
+}
---
+++ LeetCode_test/HouseRobber_198_UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class HouseRobber_198_UnitTest
+    {
+        [TestMethod,Timeout(5_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 1, 2, 3, 1 };
+            var Expected = 4;
+            var Actual = HouseRobber_198.Rob(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(5_000)]
+        public void TestMethod2()
+        {
+            var Input = new int[] { 2, 7, 9, 3, 1 };
+            var Expected = 12;
+            var Actual = HouseRobber_198.Rob(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(5_000)]
+        public void TestMethod3()
+        {
+            var Input = new int[] { 2, 1, 1, 2 };
+            var Expected = 4;
+            var Actual = HouseRobber_198.Rob(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
