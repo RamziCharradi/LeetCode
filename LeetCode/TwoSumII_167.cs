3ad9cdb6fe9332abd623d42c031e66c4d67948c8
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-13 23:00:29 +0200

    Two Sum II - Input array is sorted Solution

---
+++ LeetCode/TwoSumII_167.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class TwoSumII_167
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+
+            HashSet<int> hs = new HashSet<int>(numbers); 
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (hs.Contains(target - numbers[i]))
+                {
+                    return new int[] { i+1, Array.IndexOf(numbers, target - numbers[i]) +1 };
+                }
+            }
+
+            return new int[] {};
+        }
+    }
+}
---
+++ LeetCode_test/TwoSumII_167_UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class TwoSumII_167_UnitTest
+    {
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input_Array = new int[] { 2, 7, 11, 15 };
+            var Input_Target = 9;
+            var Expected = new int[] { 1, 2 };
+            var Actual = TwoSumII_167.TwoSum(Input_Array, Input_Target);
+            CollectionAssert.AreEqual(Expected, Actual);
+
+        }
+    }
+}
