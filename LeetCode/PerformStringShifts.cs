78a8e636516814ed3aec6ce39c204c561df69ff2
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-15 21:21:56 +0200

    Perform String Shifts

---
+++ LeetCode/PerformStringShifts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class PerformStringShifts
+    {
+        public static string StringShift(string s, int[][] shift)
+        {
+            List<char> l = s.ToList();
+
+            foreach (var direction in shift)
+            {
+                int i = 0;
+                while (i < direction[1])
+                {
+                    Shift(l, direction[0]);
+                    i++;
+                }
+
+            }
+
+            return new String(l.ToArray());
+        }
+
+        public static void Shift(List<char> list, int direction)
+        {
+            
+            if(direction == 0)
+            {
+                //abc
+                char c = list[0];
+                list.RemoveAt(0);
+                list.Add(c);
+                //bca
+            }
+            else
+            {
+                char c = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1);
+                list.Insert(0, c);
+            }
+        }
+
+    }
+}
---
+++ LeetCode_test/PerformStringShifts_UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class PerformStringShifts_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input_S = "abc";
+            var Input_Shift = new int[][] { new int[]{ 0, 1 }, new int[] { 1, 2 } };
+            var Expected = "cab";
+            var Actual = PerformStringShifts.StringShift(Input_S, Input_Shift);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
