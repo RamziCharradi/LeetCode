27e841bfd15f977358e3e6fd28bb42f0fb3514ec
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-17 20:04:50 +0200

    1408. String Matching in an Array Solution

---
+++ LeetCode/StringMatchingInAnArray_1408.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class StringMatchingInAnArray_1408
+    {
+        public static IList<string> StringMatching(string[] words)
+        {
+            IList<string> Result = new List<string>();
+
+            for (int i = 0; i < words.Length ; i++)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if(words[i].Length<words[j].Length && words[i] != words[j])
+                    {
+                        if (words[j].Contains(words[i])) Result.Add(words[i]);
+                    }
+                }
+            }
+            return Result.Distinct().ToList();
+        }
+    }
+}
---
+++ LeetCode_test/StringMatchingInAnArray_1408_UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System.Linq;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class StringMatchingInAnArray_1408_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void Testmethod1()
+        {
+            var Input = new string[]{ "mass","as","hero","superhero"};
+            IList<string> Expected = new List<string> { "as", "hero" };
+            var Actual = StringMatchingInAnArray_1408.StringMatching(Input);
+            CollectionAssert.AreEqual(Expected.ToArray(), Actual.ToArray());
+        }
+    }
+}
