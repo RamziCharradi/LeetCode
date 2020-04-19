c96650013f28409cb65c1c484d5fe872f98ee87a
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-19 15:45:04 +0200

    33. Search in Rotated Sorted Array Solution

---
+++ LeetCode/SearchInRotatedSortedArray_33.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class SearchInRotatedSortedArray_33
+    {
+        public static int Search(int[] nums, int target)
+        {
+            int[] sorted = new int[nums.Length];
+            nums.CopyTo(sorted,0);
+            Array.Sort(sorted);
+            if (!sorted.Contains(target)) return -1;
+
+            var rank = Array.IndexOf(sorted,target);
+            var Rotation = Array.IndexOf(nums,sorted[0]);
+
+            //if (rank + Rotation >= nums.Length) return (rank + Rotation) % nums.Length;
+            //return rank + Rotation;
+            return (rank + Rotation) % nums.Length;
+        }
+    }
+}
---
+++ LeetCode_test/SearchInRotatedSortedArray_33_UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class SearchInRotatedSortedArray_33_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMethod1()
+        {
+            var Input_nums = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+            var Input_target = 0;
+            var Expected = 4;
+            var Actual = SearchInRotatedSortedArray_33.Search(Input_nums, Input_target);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(20_000)]
+        public void TestMethod2()
+        {
+            var Input_nums = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+            var Input_target = 3;
+            var Expected = -1;
+            var Actual = SearchInRotatedSortedArray_33.Search(Input_nums, Input_target);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
