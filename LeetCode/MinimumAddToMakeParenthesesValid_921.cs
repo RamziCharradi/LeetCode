8521b853fbe90e40addf2f8cb40acf9ce980bf6f
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-19 17:41:47 +0200

    921. Minimum Add to Make Parentheses Valid Solution

---
+++ LeetCode/MinimumAddToMakeParenthesesValid_921.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class MinimumAddToMakeParenthesesValid_921
+    {
+        public static int MinAddToMakeValid(string S)
+        {
+            Stack<char> st = new Stack<char>();
+
+            foreach (var c in S)
+            {
+                if (st.Count == 0) st.Push(c);
+                else if (st.Peek() == '(' && c == ')') st.Pop();
+                else st.Push(c);
+            }
+
+            return st.Count;
+        }
+    }
+}
---
+++ LeetCode_test/MinimumAddToMakeParenthesesValid_921_UnitTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class MinimumAddToMakeParenthesesValid_921_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void Testmethod1()
+        {
+            var Input = "())";
+            var Expected = 1;
+            var Actual = MinimumAddToMakeParenthesesValid_921.MinAddToMakeValid(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void Testmethod2()
+        {
+            var Input = "(((";
+            var Expected = 3;
+            var Actual = MinimumAddToMakeParenthesesValid_921.MinAddToMakeValid(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void Testmethod3()
+        {
+            var Input = "()";
+            var Expected = 0;
+            var Actual = MinimumAddToMakeParenthesesValid_921.MinAddToMakeValid(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void Testmethod4()
+        {
+            var Input = "()))((";
+            var Expected = 4;
+            var Actual = MinimumAddToMakeParenthesesValid_921.MinAddToMakeValid(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
