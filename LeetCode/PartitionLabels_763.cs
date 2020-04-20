d08c09ebe306902c8581eef67898df90ef6a509b
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-20 08:47:00 +0200

    763. Partition Labels Solution

---
+++ LeetCode/PartitionLabels_763.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class PartitionLabels_763
+    {
+        public static IList<int> PartitionLabels(string S)
+        {
+            IList<int> Result = new List<int>();
+            var arr = S.ToCharArray();
+
+            int counter = 1;
+            int start = 0;
+            int index = 0;
+            if (Array.LastIndexOf(arr, arr[0]) == S.Length) return new List<int>() { S.Length };
+
+            
+            while (index < S.Length && start<S.Length)
+            {
+                index = Array.LastIndexOf(arr, arr[start]);
+                test:
+                if (arr.Skip(start).Take(index + 1 - start).Distinct().Count() != counter)
+                {
+                    counter = 0;
+                    foreach (var c in arr.Skip(start).Take(index + 1 - start ).Distinct())
+                    {
+                        index = Math.Max(index, Array.LastIndexOf(arr, c));
+                        counter++;
+                    }
+                    goto test;
+                }
+                else
+                {
+                    Result.Add(index + 1 -start);
+                    start = index + 1;
+                    counter = 1;
+                }
+            }
+
+
+
+            return Result;
+        }
+    }
+}
---
+++ LeetCode_test/PartitionLabels_763_UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System.Linq;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class PartitionLabels_763_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMthod1()
+        {
+            var Input = "ababcbacadefegdehijhklij";
+            IList<int> Expected = new List<int>() { 9, 7, 8 };
+            var Actual = PartitionLabels_763.PartitionLabels(Input);
+            CollectionAssert.AreEqual(Expected.ToArray(), Actual.ToArray());
+
+        }
+    }
+}
