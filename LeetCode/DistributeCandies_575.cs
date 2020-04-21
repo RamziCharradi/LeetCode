aef099fdc567707b92f8bf9bef261a712015f263
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-21 11:48:11 +0200

    Distribute Candies

---
+++ LeetCode/DistributeCandies_575.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class DistributeCandies_575
+    {
+        public static int DistributeCandies(int[] candies)
+        {
+            return Math.Min(candies.Count() / 2, candies.Distinct().Count());
+
+            //return (candies.Count() /2 >= candies.Distinct().Count() ) 
+            //        ? candies.Distinct().Count() 
+            //        : candies.Count()/2;
+        }
+    }
+}
---
+++ LeetCode_test/DistributeCandies_575_UnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class DistributeCandies_575_UnitTest
+    {
+        [TestMethod,Timeout(20_000)]
+        public void TestMthod1()
+        {
+            var Input = new int[] { 1, 1, 2, 2, 3, 3 };
+            var Expected = 3;
+            var Actual = DistributeCandies_575.DistributeCandies(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMthod2()
+        {
+            var Input = new int[] { 1, 1, 2, 3 };
+            var Expected = 2;
+            var Actual = DistributeCandies_575.DistributeCandies(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(20_000)]
+        public void TestMthod3()
+        {
+            var Input = new int[] { 1, 1 };
+            var Expected = 1;
+            var Actual = DistributeCandies_575.DistributeCandies(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
