d48cd70ad6b94051d476672c6ab6a7df6add1cf4
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-23 21:06:15 +0200

    13. Roman to Integer

---
+++ LeetCode/RomanToInteger_13.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class RomanToInteger_13
+    {
+        public static int RomanToInt(string s)
+        {
+            var n = s.Length;
+            var romanAndInt = new Dictionary<char, int>()
+                {
+                    {'I', 1},
+                    {'V', 5},
+                    {'X', 10},
+                    {'L', 50},
+                    {'C', 100},
+                    {'D', 500},
+                    {'M', 1000}
+                };
+
+            var result = romanAndInt[s[n - 1]];
+
+            for (int i = s.Length - 2; i >= 0; i--)
+            {
+                var cur = romanAndInt[s[i]];
+                var post = romanAndInt[s[i + 1]];
+
+                if (cur < post)
+                {
+                    result -= cur;
+                }
+                else
+                {
+                    result += cur;
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ LeetCode_test/RomanToInteger_13_UnitTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class RomanToInteger_13_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMtheod1()
+        {
+            var Input = "III";
+            var Expected = 3;
+            var Actual = RomanToInteger_13.RomanToInt(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMtheod2()
+        {
+            var Input = "IV";
+            var Expected = 4;
+            var Actual = RomanToInteger_13.RomanToInt(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod, Timeout(2_000)]
+        public void TestMtheod3()
+        {
+            var Input = "IX";
+            var Expected = 9;
+            var Actual = RomanToInteger_13.RomanToInt(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMtheod4()
+        {
+            var Input = "LVIII";
+            var Expected = 58;
+            var Actual = RomanToInteger_13.RomanToInt(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMtheod5()
+        {
+            var Input = "MCMXCIV";
+            var Expected = 1994;
+            var Actual = RomanToInteger_13.RomanToInt(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMtheod6()
+        {
+            var Input = "I";
+            var Expected = 1;
+            var Actual = RomanToInteger_13.RomanToInt(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
