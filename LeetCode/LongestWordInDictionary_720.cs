5a3874fc7bc75e3385db85c066bb4f3278e47cad
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-23 20:35:53 +0200

    Longest Word Solution

---
+++ LeetCode/LongestWordInDictionary_720.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class LongestWordInDictionary_720
+    {
+        public static string LongestWord(string[] words)
+        {
+            if (words == null) return null;
+            Array.Sort(words);
+            var visitedPrefix = new HashSet<string>();
+            string maximumWord = "";
+
+            foreach (var word in words)
+            {
+                var length = word.Length;
+
+                if (length == 1 || visitedPrefix.Contains(word.Substring(0, length - 1)))
+                {
+                    maximumWord = word.Length > maximumWord.Length ? word : maximumWord;
+                    visitedPrefix.Add(word);
+                }
+            }
+            return maximumWord;
+
+
+            //if (words.Length == 1) return words[0];
+            //string Result = "";
+
+            //var g1 = words.GroupBy(x => x).ToDictionary(x=>x.Key,x=>x.Key.Length)
+            //    .OrderByDescending(x => x.Value)
+            //    .ThenBy(x => x.Key)
+            //    .Select(x => x.Key);
+
+            //foreach (var w in g1)
+            //{
+            //    bool test = true;
+            //    int counter = w.Length;
+            //    if (w.Length > Result.Length)
+            //    {
+            //        foreach (var x in g1.Where(x=>x[0]==w[0]))
+            //        {
+            //            if(x.Length < w.Length && w.Substring(0,x.Length) == x)
+            //            {
+            //                if (!w.Contains(x))
+            //                {
+            //                    test = false;
+            //                    break;
+            //                }
+            //                else
+            //                {
+            //                    counter--;
+            //                }
+            //            }
+            //        }
+            //        if (test && counter==1 && w.Length>Result.Length) return w;
+            //    }
+
+            //}
+            //return Result;
+        }
+    }
+}
---
+++ LeetCode_test/LongestWordInDictionary_720_UnitTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class LongestWordInDictionary_720_UnitTest
+    {
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new string[] { "w", "wo", "wor", "worl", "world" };
+            var Expected = "world";
+            var Actual = LongestWordInDictionary_720.LongestWord(Input);
+            Assert.AreEqual(Expected, Actual);
+        
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input = new string[] { "a", "banana", "app", "appl", "ap", "apply", "apple" };
+            var Expected = "apple";
+            var Actual = LongestWordInDictionary_720.LongestWord(Input);
+            Assert.AreEqual(Expected, Actual);
+
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input = new string[] { "b", "br", "bre", "brea", "break", "breakf", "breakfa", "breakfas", "breakfast", "l", "lu", "lun", "lunc", "lunch", "d", "di", "din", "dinn", "dinne", "dinner" };
+            var Expected = "breakfast";
+            var Actual = LongestWordInDictionary_720.LongestWord(Input);
+            Assert.AreEqual(Expected, Actual);
+
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod4()
+        {
+            var Input = new string[] { "rac", "rs", "ra", "on", "r", "otif", "o", "onpdu", "rsf", "rs", "ot", "oti", "racy", "onpd" };
+            var Expected = "otif";
+            var Actual = LongestWordInDictionary_720.LongestWord(Input);
+            Assert.AreEqual(Expected, Actual);
+
+        }
+
+    }
+}
