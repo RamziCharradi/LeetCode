a1ce7ca13009e85c3d4c381bf0a0e8e3cc5ba96e
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-24 23:43:46 +0200

    Non_decreasing_Array

---
+++ LeetCode/Non_decreasing_Array_655.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Non_decreasing_Array_655
+    {
+        public static bool CheckPossibility(int[] nums)
+        {
+            int counter = 0;
+            for (int i = 1; i < nums.Length-1; i++)
+            {
+                
+                if (nums[i - 1] > nums[i])
+                {
+                    if (nums[i - 1] > nums[i + 1] && counter>0) return false;
+                    if (nums[i] < nums[i + 1]) counter++;
+                    else return false;
+                }
+                if (nums[nums.Length-2] > nums[nums.Length-1])
+                {
+                    
+                    if (counter > 0) return false;
+                    else counter++;
+                }
+            }
+
+            return (counter == 1);
+        }
+    }
+}
---
+++ LeetCode_test/Non_decreasing_Array_655_UnitTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class Non_decreasing_Array_655_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 4, 2, 3 };
+            var Expected = true;
+            var Actual = Non_decreasing_Array_655.CheckPossibility(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input = new int[] { 4, 2, 1 };
+            var Expected = false;
+            var Actual = Non_decreasing_Array_655.CheckPossibility(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input = new int[] {-1, 4, 2, 3 };
+            var Expected = true;
+            var Actual = Non_decreasing_Array_655.CheckPossibility(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod4()
+        {
+            var Input = new int[] { -1, 4, 2, 3 ,1};
+            var Expected = false;
+            var Actual = Non_decreasing_Array_655.CheckPossibility(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod5()
+        {
+            var Input = new int[] { -1, 4, 2, 3, 1,  7 };
+            var Expected = false;
+            var Actual = Non_decreasing_Array_655.CheckPossibility(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod6()
+        {
+            var Input = new int[] { 3, 4, 2, 3 };
+            var Expected = false;
+            var Actual = Non_decreasing_Array_655.CheckPossibility(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
