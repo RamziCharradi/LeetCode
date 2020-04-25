6857797ff1eaff281a1e678460987d7821c835ca
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-25 21:45:58 +0200

    Maximum Depth of N-ary Tree 559 Solution

---
+++ LeetCode/MaximumDepthOfN-aryTree_559.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class MaximumDepthOfN_aryTree_559
+    {
+        public static int MaxDepth(Node root)
+        {
+
+            if (root == null) return 0;
+
+            int m = 0;
+
+            if (root.children == null) return 1+ m;
+
+            foreach (var node in root.children)
+            {
+                var k = MaxDepth(node);
+                if (k >= m) m = k;
+            }
+
+            return 1 + m;
+        }
+    }
+}
---
+++ LeetCode_test/MaximumDepthOfN_aryTree_559_UnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class MaximumDepthOfN_aryTree_559_UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var Input = new Node(1)
+            {
+                children = new List<Node>
+                {
+                   new Node(3)
+                   {
+                       children = new List<Node>
+                       {
+                           new Node(5),
+                           new Node(6)
+                       }
+                   },
+                   new Node(2),
+                   new Node(4)
+                }
+            };
+
+            var Expected = 3;
+            var Actual = MaximumDepthOfN_aryTree_559.MaxDepth(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
