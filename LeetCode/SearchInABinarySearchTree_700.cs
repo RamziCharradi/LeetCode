70fdda36398b960ad87058f11253aa4e35c25cfd
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-25 11:49:55 +0200

    Search in a Binary Search Tree Solution

---
+++ LeetCode/SearchInABinarySearchTree_700.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class SearchInABinarySearchTree_700
+    {
+        public static TreeNode SearchBST(TreeNode root, int val)
+        {
+            while (root != null)
+            {
+                if (root == null || (root.val == val))
+                    return root;
+                else if (root.val < val)
+                    root = root.right;
+                else root = root.left;
+            }
+
+            return root;
+        }
+    }
+}
---
+++ LeetCode_test/SearchInABinarySearchTree_700_UnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using LeetCode_test.Helpers;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class SearchInABinarySearchTree_700_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input_root = new TreeNode(4)
+            {
+                left = new TreeNode(2) 
+                {
+                    left = new TreeNode(1),
+                    right = new TreeNode(3)
+                },
+                right = new TreeNode(7)
+            };
+            var Input_val = 2;
+            var Expected = new TreeNode(2)
+            {
+                left = new TreeNode(1),
+                right = new TreeNode(3)
+            };
+            var Actual = SearchInABinarySearchTree_700.SearchBST(Input_root, Input_val);
+            Assert.IsTrue(Helper<int>.HaveSameInorderSequenceForTwoTreeNodes(Expected, Actual));
+
+        }
+
+    }
+}
