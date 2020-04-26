ad6c3885758b381fe29ceecbcf52d610658bd57a
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-26 19:18:03 +0200

    Island Perimeter Solution

---
+++ LeetCode/IslandPerimeter_463.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class IslandPerimeter_463
+    {
+        public static int IslandPerimeter(int[][] grid)
+        {
+            int Result = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length ; j++)
+                {
+                    
+                    int counter = 0;
+                    if (i - 1 >= 0 && grid[i - 1][j] == 1) counter++;
+                    if (i + 1 < grid.Length && grid[i + 1][j] == 1) counter++;
+                    if (j - 1 >= 0 && grid[i][j - 1] == 1) counter++;
+                    if(j + 1 < grid[0].Length && grid[i][j + 1] == 1) counter++;
+
+                    if (grid[i][j] == 1) Result += 4 - counter;
+
+                }
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ LeetCode_test/IslandPerimeter_463_UnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class IslandPerimeter_463_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[][]
+            {
+                new int[]{0,1,0,0},
+                new int[]{1,1,1,0},
+                new int[]{0,1,0,0},
+                new int[]{ 1, 1, 0, 0 }
+            };
+            var Expected = 16;
+            var Actual = IslandPerimeter_463.IslandPerimeter(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
