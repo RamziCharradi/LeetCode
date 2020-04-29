0befb2b7bc7e79c83f211e311d8a517f014dce5f
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-04-29 15:21:54 +0200

    Increasing Order Search Tree Solution

---
+++ LeetCode/IncreasingOrderSearchTree_897.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class IncreasingOrderSearchTree_897
+    {
+        public static TreeNode IncreasingBST(TreeNode root)
+        {
+            TreeNode output = null, current = null;
+            Helper(root, ref output, ref current);
+            return output;
+
+        }
+        private static void Helper(TreeNode inputNode, ref TreeNode outputRoot, ref TreeNode outputCurrent)
+        {
+            if (inputNode == null)
+            {
+                return;
+            }
+
+            Helper(inputNode.left, ref outputRoot, ref outputCurrent);
+
+            if (outputRoot == null)
+            {
+                outputRoot = new TreeNode(inputNode.val);
+                outputCurrent = outputRoot;
+            }
+            else
+            {
+                outputCurrent.right = new TreeNode(inputNode.val);
+                outputCurrent = outputCurrent.right;
+            }
+
+            Helper(inputNode.right, ref outputRoot, ref outputCurrent);
+        }
+
+       
+    }
+}
---
+++ LeetCode_test/IncreasingOrderSearchTree_897_UnitTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using LeetCode_test.Helpers;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class IncreasingOrderSearchTree_897_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new TreeNode(5)
+            {
+                left = new TreeNode(3)
+                {
+                    left = new TreeNode(2)
+                    {
+                        left = new TreeNode(1)
+                    },
+                    right = new TreeNode(4)
+                },
+                right = new TreeNode(6)
+                {
+                    right = new TreeNode(8)
+                    {
+                        left = new TreeNode(7),
+                        right = new TreeNode(9)
+                    }
+                }
+            };
+            var Expected = new TreeNode(1)
+            {
+                right = new TreeNode(2)
+                {
+                    right = new TreeNode(3)
+                    {
+                        right = new TreeNode(4)
+                        {
+                            right = new TreeNode(5)
+                            {
+                                right = new TreeNode(6)
+                                {
+                                    right = new TreeNode(7)
+                                    {
+                                        right = new TreeNode(8)
+                                        {
+                                            right = new TreeNode(9)
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            var Actual = IncreasingOrderSearchTree_897.IncreasingBST(Input);
+            Assert.IsTrue(Helper<int>.HaveSameInorderSequenceForTwoTreeNodes(Expected, Actual));
+
+        }
+    }
+}
