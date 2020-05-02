ba34abde0d94c013a857a1a0998579950a89e167
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-02 13:36:29 +0200

    Is Monotonic Solution

---
+++ LeetCode/MonotonicArray_896.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class MonotonicArray_896
+    {
+        public static bool IsMonotonic(int[] A)
+        {
+
+            //return A.OrderBy(x => x).ToArray().SequenceEqual(A) || A.OrderByDescending(x => x).ToArray().SequenceEqual(A);
+
+            var length = A.Length;
+            if (length < 3)
+            {
+                return true;
+            }
+
+            if (A[0] < A[length - 1])
+            {
+                for (int i = 1; i < length; i++)
+                {
+                    if (A[i - 1] > A[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 1; i < length; i++)
+                {
+                    if (A[i - 1] < A[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode_test/MonotonicArray_896_UnitTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class MonotonicArray_896_UnitTest
+    {
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 1, 2, 2, 3 };
+            var Expected = true;
+            var Actual = MonotonicArray_896.IsMonotonic(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input = new int[] { 6, 5, 4, 4 };
+            var Expected = true;
+            var Actual = MonotonicArray_896.IsMonotonic(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input = new int[] { 1, 3, 2 };
+            var Expected = false;
+            var Actual = MonotonicArray_896.IsMonotonic(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod4()
+        {
+            var Input = new int[] { 1, 2, 4, 5 };
+            var Expected = true;
+            var Actual = MonotonicArray_896.IsMonotonic(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod5()
+        {
+            var Input = new int[] { 1, 1, 1 };
+            var Expected = true;
+            var Actual = MonotonicArray_896.IsMonotonic(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
