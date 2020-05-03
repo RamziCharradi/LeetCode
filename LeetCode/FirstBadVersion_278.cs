66b7663866fa42c021efa247980cb30a3836d22a
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-03 13:15:09 +0200

    First Bad Version Solution

---
+++ LeetCode/FirstBadVersion_278.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class FirstBadVersion_278
+    {
+        public static int FirstBadVersion(int n)
+        {
+            int left = 0;
+            int right = n;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (IsBadVersion(mid))
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return left;
+
+        }
+
+        static bool IsBadVersion(int version)
+        {
+            if (version == 4) return true;
+            return false;
+        }
+    }
+}
---
+++ LeetCode_test/FirstBadVersion_278_UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class FirstBadVersion_278_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = 5;
+            var Expected = 4;
+            var Actual = FirstBadVersion_278.FirstBadVersion(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
