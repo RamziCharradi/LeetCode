3836b4c6ed8da4ccab9a598d9664ed7e70ce2b48
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-04 07:57:47 +0200

    Is Subsequence Solution

---
+++ LeetCode/IsSubsequence_392.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class IsSubsequence_392
+    {
+        public static bool IsSubsequence(string s, string t)
+        {
+            if (s.Length == 0) return true;
+            if (s.Length > t.Length) return false;
+            int curr = 0;
+            int counter = 0;
+            while (curr < t.Length && counter<s.Length)
+            {
+                if (s[counter] == t[curr]) counter++;
+                curr++;
+            }
+
+            return counter == s.Length;
+        }
+    }
+}
---
+++ LeetCode_test/IsSubsequence_392_UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class IsSubsequence_392_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input_s = "abc";
+            var Input_t = "ahbgdc";
+            var Expected = true;
+            var Actual = IsSubsequence_392.IsSubsequence(Input_s, Input_t);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input_s = "axc";
+            var Input_t = "ahbgdc";
+            var Expected = false;
+            var Actual = IsSubsequence_392.IsSubsequence(Input_s, Input_t);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input_s = "";
+            var Input_t = "ahbgdc";
+            var Expected = true;
+            var Actual = IsSubsequence_392.IsSubsequence(Input_s, Input_t);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
