5190690a33daef23c694b40545ef4502446d12af
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-06 09:24:18 +0200

    83. Remove Duplicates from Sorted List Solution

---
+++ LeetCode/RemoveDuplicatesFromSortedList_83.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class RemoveDuplicatesFromSortedList_83
+    {
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            var root = head;
+            while (head != null)
+            {
+
+                var pre = head;
+
+                while (head != null && head.val == pre.val)
+                {
+                    head = head.next;
+                    pre.next = head;
+                }
+
+            }
+            return root;
+        }
+    }
+}
---
+++ LeetCode_test/RemoveDuplicatesFromSortedList_83_UnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class RemoveDuplicatesFromSortedList_83_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new ListNode(1)
+            {
+                next = new ListNode(1)
+                {
+                    next = new ListNode(2)
+                }
+            };
+            var Expected = new ListNode(1)
+            {
+                next = new ListNode(2)
+            };
+            var Actual = RemoveDuplicatesFromSortedList_83.DeleteDuplicates(Input);
+            while (Expected != null && Actual != null)
+            {
+                Assert.AreEqual(Expected.val, Actual.val);
+                Expected = Expected.next;
+                Actual = Actual.next;
+            }
+        }
+    }
+}
