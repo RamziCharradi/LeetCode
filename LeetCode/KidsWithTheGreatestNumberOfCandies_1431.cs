c077001cc6998224c248ebeda791198cc1fa637b
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-08 16:42:16 +0200

    1431. Kids With the Greatest Number of Candies Solution

---
+++ LeetCode/KidsWithTheGreatestNumberOfCandies_1431.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class KidsWithTheGreatestNumberOfCandies_1431
+    {
+        public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            IList<bool> Result = new List<bool> { };
+            var max = candies.Max();
+
+            foreach (var c in candies) Result.Add(c + extraCandies >= max);
+            
+            return Result;
+        }
+    }
+}
---
+++ LeetCode_test/KidsWithTheGreatestNumberOfCandies_1431_UnitTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System.Linq;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class KidsWithTheGreatestNumberOfCandies_1431_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input_candies = new int[] { 2, 3, 5, 1, 3 };
+            var Input_extra = 3;
+            IList<bool> Expected = new List<bool> { true, true, true, false, true };
+            var Actual = KidsWithTheGreatestNumberOfCandies_1431.KidsWithCandies(Input_candies, Input_extra);
+            CollectionAssert.AreEqual(Expected.ToList(), Actual.ToList());
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input_candies = new int[] { 4, 2, 1, 1, 2 };
+            var Input_extra = 1;
+            IList<bool> Expected = new List<bool> { true, false, false, false, false };
+            var Actual = KidsWithTheGreatestNumberOfCandies_1431.KidsWithCandies(Input_candies, Input_extra);
+            CollectionAssert.AreEqual(Expected.ToList(), Actual.ToList());
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input_candies = new int[] { 12, 1, 12 };
+            var Input_extra = 10;
+            IList<bool> Expected = new List<bool> { true, false, true };
+            var Actual = KidsWithTheGreatestNumberOfCandies_1431.KidsWithCandies(Input_candies, Input_extra);
+            CollectionAssert.AreEqual(Expected.ToList(), Actual.ToList());
+        }
+
+    }
+}
