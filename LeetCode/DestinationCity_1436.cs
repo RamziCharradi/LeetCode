a46608a966c1169c5c4d76b4cbdcefdbb3cbe090
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-09 23:51:03 +0200

    1436. Destination City Solution

---
+++ LeetCode/DestinationCity_1436.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class DestinationCity_1436
+    {
+        public string DestCity(IList<IList<string>> paths)
+        {
+            var d = new Dictionary<string, string>();
+            foreach (var path in paths)
+                d[path[0]] = path[1];
+            return d.First(keyValue => !d.ContainsKey(keyValue.Value)).Value;
+
+        }
+    }
+}
