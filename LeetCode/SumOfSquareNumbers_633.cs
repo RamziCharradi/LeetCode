99b68a370b3ff17ffcf0d2803c33159a3823df01
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-11 13:21:57 +0200

    633. Sum of Square Numbers Solution

---
+++ LeetCode/SumOfSquareNumbers_633.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class SumOfSquareNumbers_633
+    {
+        public static bool JudgeSquareSum(int c)
+        {
+            if (c < 0)
+                return false;
+
+            int start = 0;
+            int end = (int)Math.Sqrt(c);
+            while (start <= end)
+            {
+                var smaller = start * start;
+                var bigger = end * end;
+                var sum = smaller + bigger;
+                if (sum == c)
+                    return true;
+                if (sum < c)
+                    start++;
+                else
+                    end--;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode_test/SumOfSquareNumbers_633_UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class SumOfSquareNumbers_633_UnitTest
+    {
+        [TestMethod]
+        public void TestMthod1()
+        {
+            var Input = 5;
+            var Expected = true;
+            var Actual = SumOfSquareNumbers_633.JudgeSquareSum(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod]
+        public void TestMthod2()
+        {
+            var Input = 3;
+            var Expected = false;
+            var Actual = SumOfSquareNumbers_633.JudgeSquareSum(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
