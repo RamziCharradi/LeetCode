a7535fd149e224c15b44a22f16a491597080623c
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-11 23:52:33 +0200

    Power Of - Solution

---
+++ LeetCode/PowerOfThree_326.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class PowerOfThree_326
+    {
+        public static bool IsPowerOfThree(int n)
+        {
+            if (n < 1) return false;
+
+            while (n>1)
+            {
+                if (n % 3 != 0) return false;
+                else n /= 3 ;
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode_test/PowerOfThree_326_UnitTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class PowerOfThree_326_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMthod1()
+        {
+            var Input = 27;
+            var Expected = true;
+            var Actual = PowerOfThree_326.IsPowerOfThree(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMthod2()
+        {
+            var Input = 0;
+            var Expected = false;
+            var Actual = PowerOfThree_326.IsPowerOfThree(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMthod3()
+        {
+            var Input = 9;
+            var Expected = true;
+            var Actual = PowerOfThree_326.IsPowerOfThree(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMthod4()
+        {
+            var Input = 45;
+            var Expected = false;
+            var Actual = PowerOfThree_326.IsPowerOfThree(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
