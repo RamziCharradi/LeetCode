88955c9a6113e3cdf5d75095c937409608ecc929
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-12 16:02:09 +0200

    1089. Duplicate Zeros Solution

---
+++ LeetCode/DuplicateZeros_1089.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class DuplicateZeros_1089
+    {
+        public static int[] DuplicateZeros(int[] arr)
+        {
+            var Result = new int[arr.Length];
+            var x = 0;
+            var y = 0;
+            while (y < arr.Length)
+            {
+                if (arr[x] == 0)
+                {
+                    Result[y++] = 0;
+                    Result[y++] = 0;
+                    x++;
+                }
+                else
+                {
+                    Result[y++]=arr[x++];
+                }
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ LeetCode_test/DuplicateZeros_1089_UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class DuplicateZeros_1089_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 1, 0, 2, 3, 0, 4, 5, 0 };
+            var Expected = new int[] { 1, 0, 0, 2, 3, 0, 0, 4 };
+            var Actual = DuplicateZeros_1089.DuplicateZeros(Input);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input = new int[] { 1, 2, 3 };
+            var Expected = new int[] { 1, 2, 3 };
+            var Actual = DuplicateZeros_1089.DuplicateZeros(Input);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
