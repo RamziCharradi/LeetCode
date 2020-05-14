d0f48a38eae9b8dd271676f8cb60dd6ef13effd1
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-14 15:05:45 +0200

    Rotated Digits - Solutiion

---
+++ LeetCode/RotatedDigits_788.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class RotatedDigits_788
+    {
+        public static int RotatedDigits(int N)
+        {
+
+            int count = 0;
+            for (int i = 2; i <= N; i++)
+            {
+                if (IsGood(i))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private static bool IsGood(int n)
+        {
+            bool isGood = false;
+            while (n > 0)
+            {
+                int m = n % 10;
+                if (m == 3 || m == 4 || m == 7)
+                {
+                    return false;
+                }
+                if (m == 2 || m == 5 || m == 6 || m == 9)
+                {
+                    isGood = true;
+                }
+                n /= 10;
+            }
+            return isGood;
+        }
+    }
+}
---
+++ LeetCode_test/RotatedDigits_788_UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class RotatedDigits_788_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = 10;
+            var Expected = 4;
+            var Actual = RotatedDigits_788.RotatedDigits(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
