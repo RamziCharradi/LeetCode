07afefc64404918257695b1b5d2c227292e65a97
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-15 12:48:28 +0200

    496. Next Greater Element I Solution

---
+++ LeetCode/NextGreaterElementI_496.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class NextGreaterElementI_496
+    {
+        public static int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            int[] result = new int[nums1.Length];
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                var index = Array.IndexOf(nums2, nums1[i]) + 1 ;
+                bool exist = false; 
+                while(index< nums2.Length)
+                {
+                    if (nums1[i] < nums2[index]) 
+                    {
+                        result[i] = nums2[index];
+                        exist = true;
+                        break;
+                    } 
+                    index++;
+                }
+                if (!exist) result[i] = -1;
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode_test/NextGreaterElementI_496_UnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class NextGreaterElementI_496_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input_nums1 = new int[] { 4, 1, 2 };
+            var Input_nums2 = new int[] { 1, 3, 4, 2 };
+            var Expected = new int[] { -1, 3, -1 };
+            var Actual = NextGreaterElementI_496.NextGreaterElement(Input_nums1, Input_nums2);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input_nums1 = new int[] { 2, 4 };
+            var Input_nums2 = new int[] { 1, 2, 3, 4 };
+            var Expected = new int[] { 3, -1 };
+            var Actual = NextGreaterElementI_496.NextGreaterElement(Input_nums1, Input_nums2);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input_nums1 = new int[] { 1, 3, 5, 2, 4 };
+            var Input_nums2 = new int[] { 6, 5, 4, 3, 2, 1, 7 };
+            var Expected = new int[] { 7, 7, 7, 7, 7 };
+            var Actual = NextGreaterElementI_496.NextGreaterElement(Input_nums1, Input_nums2);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+    }
+}
