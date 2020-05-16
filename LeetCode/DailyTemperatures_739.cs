cf947f436d56aa1a258668cce0e8657de5128599
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-16 10:31:11 +0200

    739. Daily Temperatures - Solution

---
+++ LeetCode/DailyTemperatures_739.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class DailyTemperatures_739
+    {
+        public static int[] DailyTemperatures(int[] T)
+        {
+            var result = new int[T.Length];
+
+            for (int i = 0; i < T.Length; i++)
+            {
+                var x = i + 1;
+                var exist = false;
+                var counter = 0;
+                while (x < T.Length)
+                {
+                    counter++;
+                    if (T[x] > T[i]) 
+                    {
+                        result[i] = counter;
+                        exist = true;
+                        break;
+                    }
+                    x++;
+                }
+                if (!exist) result[i] = 0;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode_test/DailyTemperatures_739_UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class DailyTemperatures_739_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 73, 74, 75, 71, 69, 72, 76, 73 };
+            var Expected = new int[] { 1, 1, 4, 2, 1, 1, 0, 0 };
+            var Actual = DailyTemperatures_739.DailyTemperatures(Input);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+    }
+}
