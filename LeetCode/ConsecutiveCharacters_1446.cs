f2c0b25db4950b9e285ef161f83e22525e9343d8
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-17 22:46:38 +0200

    1446. Consecutive Characters - Solution

---
+++ LeetCode/ConsecutiveCharacters_1446.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ConsecutiveCharacters_1446
+    {
+        public static int MaxPower(string s)
+        {
+            var Result = 0;
+            var counter = 0;
+            var prev = s[0];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == prev)
+                {
+                    counter++;
+                    Result = Math.Max(Result, counter);
+                }
+                else
+                {
+                    Result = Math.Max(Result, counter);
+                    counter = 1;
+                    prev = s[i];
+                }
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ LeetCode_test/ConsecutiveCharacters_1446_UnitTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class ConsecutiveCharacters_1446_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = "leetcode";
+            var Expected = 2;
+            var Actual = ConsecutiveCharacters_1446.MaxPower(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input = "abbcccddddeeeeedcba";
+            var Expected = 5;
+            var Actual = ConsecutiveCharacters_1446.MaxPower(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input = "triplepillooooow";
+            var Expected = 5;
+            var Actual = ConsecutiveCharacters_1446.MaxPower(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod4()
+        {
+            var Input = "hooraaaaaaaaaaay";
+            var Expected = 11;
+            var Actual = ConsecutiveCharacters_1446.MaxPower(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod5()
+        {
+            var Input = "tourist";
+            var Expected = 1;
+            var Actual = ConsecutiveCharacters_1446.MaxPower(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod6()
+        {
+            var Input = "cc";
+            var Expected = 2;
+            var Actual = ConsecutiveCharacters_1446.MaxPower(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod7()
+        {
+            var Input = "c";
+            var Expected = 1;
+            var Actual = ConsecutiveCharacters_1446.MaxPower(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
