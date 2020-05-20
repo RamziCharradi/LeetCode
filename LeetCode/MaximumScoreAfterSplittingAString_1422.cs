1cb0c889d945a5c0c330adc2d294d89fea956ff8
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-21 01:28:28 +0200

    1422. Maximum Score After Splitting a String - Solution

---
+++ LeetCode/MaximumScoreAfterSplittingAString_1422.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class MaximumScoreAfterSplittingAString_1422
+    {
+        public static int MaxScore(string s)
+        {
+            var right = s.Where(x=>x=='1').Count();
+            var left = 0;
+            var Result = 0;
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                if (s[i] == '0') left++;
+                else right--;
+
+                Result = Math.Max(Result, right + left);
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ LeetCode_test/MaximumScoreAfterSplittingAString_1422_UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class MaximumScoreAfterSplittingAString_1422_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = "011101";
+            var Expected = 5;
+            var Actual = MaximumScoreAfterSplittingAString_1422.MaxScore(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input = "00111";
+            var Expected = 5;
+            var Actual = MaximumScoreAfterSplittingAString_1422.MaxScore(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input = "1111";
+            var Expected = 3;
+            var Actual = MaximumScoreAfterSplittingAString_1422.MaxScore(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
