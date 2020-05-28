fabc6513b5427e8e3219eefd8abde8f5d9e58667
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-28 14:57:44 +0200

    1184. Distance Between Bus Stops - Solution

---
+++ LeetCode/DistanceBetweenBusStops_1184.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class DistanceBetweenBusStops_1184
+    {
+        public static int DistanceBetweenBusStops(int[] distance, int start, int destination)
+        {
+            int liMin = Math.Min(start, destination);
+            int liMax = Math.Max(start, destination);
+
+            int liSumOfMax = distance.Skip(liMin).Take(liMax - liMin).Sum();
+
+            return Math.Min(liSumOfMax, distance.Sum() - liSumOfMax);
+
+        }
+    }
+}
---
+++ LeetCode_test/DistanceBetweenBusStops_1184_UnitTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class DistanceBetweenBusStops_1184_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input_distance = new int[] { 1, 2, 3, 4 };
+            var Input_start = 0;
+            var Input_destination = 1;
+            var Expected = 1;
+            var Actual = DistanceBetweenBusStops_1184.DistanceBetweenBusStops(Input_distance, Input_start, Input_destination);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input_distance = new int[] { 1, 2, 3, 4 };
+            var Input_start = 0;
+            var Input_destination = 2;
+            var Expected = 3;
+            var Actual = DistanceBetweenBusStops_1184.DistanceBetweenBusStops(Input_distance, Input_start, Input_destination);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input_distance = new int[] { 1, 2, 3, 4 };
+            var Input_start = 0;
+            var Input_destination = 3;
+            var Expected = 4;
+            var Actual = DistanceBetweenBusStops_1184.DistanceBetweenBusStops(Input_distance, Input_start, Input_destination);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod4()
+        {
+            /*
+            [7,10,1,12,11,14,5,0]
+                7
+                2 
+            
+             */
+            var Input_distance = new int[] { 7, 10, 1, 12, 11, 14, 5, 0 };
+            var Input_start = 7;
+            var Input_destination = 2;
+            var Expected = 17;
+            var Actual = DistanceBetweenBusStops_1184.DistanceBetweenBusStops(Input_distance, Input_start, Input_destination);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
