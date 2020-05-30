3cae74140e68784fc6b3542126267652931e9e32
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-30 12:51:44 +0200

    581. Shortest Unsorted Continuous Subarray - Solution

---
+++ LeetCode/ShortestUnsortedContinuousSubarray_581.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ShortestUnsortedContinuousSubarray_581
+    {
+        public static int FindUnsortedSubarray(int[] nums)
+        {
+            int[] Sorted = new int[nums.Length];
+            Array.Copy(nums, Sorted, nums.Length);
+            Array.Sort(Sorted);
+
+            // int[] Sorted = nums.OrderBy(x => x).ToArray();
+            int start = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != Sorted[i])
+                {
+                    start = i - 1;
+                    break;
+                }
+            }
+            int Finish = 0;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (nums[i] != Sorted[i])
+                {
+                    Finish = i;
+                    break;
+                }
+            }
+
+            return Finish - start;
+        }
+    }
+}
---
+++ LeetCode_test/ShortestUnsortedContinuousSubarray_581_UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class ShortestUnsortedContinuousSubarray_581_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 2, 6, 4, 8, 10, 9, 15 };
+            var Expected = 5;
+            var Actual = ShortestUnsortedContinuousSubarray_581.FindUnsortedSubarray(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
