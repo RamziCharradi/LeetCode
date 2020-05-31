9c4777fbae80edfcc541b18fbbf0b4f753803f29
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-05-31 16:04:00 +0200

    1466. Reorder Routes to Make All Paths Lead to the City Zero - Solution

---
+++ LeetCode/ReorderRoutesToMakeAllPathsLeadToTheCityZero_1466.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ReorderRoutesToMakeAllPathsLeadToTheCityZero_1466
+    {
+        public static int MinReorder(int n, int[][] connections)
+        {
+            var counter = 0;
+            HashSet<int> hs = new HashSet<int>();
+            foreach (var x in connections)
+            {
+                if (x.Contains(0)) 
+                {
+                    if (x[0] == 0)
+                    {
+                        counter++;
+                        hs.Add(x[1]);
+                    }
+                    else
+                    {
+                        hs.Add(x[0]);
+                    }
+                }
+                else
+                {
+                    if (!hs.Add(x[0])) 
+                    { 
+                        counter++;
+                        hs.Add(x[1]);
+                    }
+
+                }
+
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ LeetCode_test/ReorderRoutesToMakeAllPathsLeadToTheCityZero_1466_UnitTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class ReorderRoutesToMakeAllPathsLeadToTheCityZero_1466_UnitTest
+    {
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input_n = 6;
+            var Input_connections = new int[][]
+            {
+                new int[]{0,1},
+                new int[]{1,3},
+                new int[]{2,3},
+                new int[]{4,0},
+                new int[]{4,5}
+            };
+            var Expected = 3;
+            var Actual = ReorderRoutesToMakeAllPathsLeadToTheCityZero_1466.MinReorder(Input_n, Input_connections);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input_n = 5;
+            var Input_connections = new int[][]
+            {
+                // [[1,0],[1,2],[3,2],[3,4]]
+                new int[]{1,0},
+                new int[]{1,2},
+                new int[]{3,2},
+                new int[]{3,4}
+            };
+            var Expected = 2;
+            var Actual = ReorderRoutesToMakeAllPathsLeadToTheCityZero_1466.MinReorder(Input_n, Input_connections);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input_n = 3;
+            var Input_connections = new int[][]
+            {
+                // [[1,0],[2,0]]
+                new int[]{1,0},
+                new int[]{2,0}
+            };
+            var Expected = 0;
+            var Actual = ReorderRoutesToMakeAllPathsLeadToTheCityZero_1466.MinReorder(Input_n, Input_connections);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod4()
+        {
+            var Input_n = 5;
+            var Input_connections = new int[][]
+            {
+                // [[1,0],[1,2],[2,3],[4,2]]
+                new int[]{1,0},
+                new int[]{1,2},
+                new int[]{2,3},
+                new int[]{4,2}
+            };
+            var Expected = 2;
+            var Actual = ReorderRoutesToMakeAllPathsLeadToTheCityZero_1466.MinReorder(Input_n, Input_connections);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
