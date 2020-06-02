ca5d02e28fa991a232ff5b47bb573fcb2a565fe2
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-06-02 17:56:01 +0200

    1222. Queens That Can Attack the King - Solution

---
+++ LeetCode/QueensThatCanAttackTheKing_1222.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class QueensThatCanAttackTheKing_1222
+    {
+		public static IList<IList<int>> QueensAttacktheKing(int[][] queens, int[] king)
+		{
+
+			bool[,] board = new bool[8, 8];
+			List<IList<int>> result = new List<IList<int>>();
+
+			for (int i = 0; i < queens.Length; i++)
+			{
+				board[queens[i][0], queens[i][1]] = true;
+			}
+
+
+			int x = king[0];
+			int y = king[1];
+
+			// right
+			for (int i = x + 1; i < 8; i++)
+			{
+				if (board[i, y])
+				{
+					result.Add(new List<int>() { i, y });
+					break;
+				}
+			}
+
+			// left
+			for (int i = x - 1; i >= 0; i--)
+			{
+				if (board[i, y])
+				{
+					result.Add(new List<int>() { i, y });
+					break;
+				}
+			}
+
+			// top
+			for (int i = y + 1; i < 8; i++)
+			{
+				if (board[x, i])
+				{
+					result.Add(new List<int>() { x, i });
+					break;
+				}
+			}
+
+			// bottom
+			for (int i = y - 1; i >= 0; i--)
+			{
+				if (board[x, i])
+				{
+					result.Add(new List<int>() { x, i });
+					break;
+				}
+			}
+
+
+			// diagonal top left
+			for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--)
+			{
+				if (board[i, j])
+				{
+					result.Add(new List<int>() { i, j });
+					break;
+				}
+			}
+
+			// diagonal top right
+			for (int i = x - 1, j = y + 1; i >= 0 && j < 8; i--, j++)
+			{
+				if (board[i, j])
+				{
+					result.Add(new List<int>() { i, j });
+					break;
+				}
+			}
+
+			// diagonal bottom left
+			for (int i = x + 1, j = y - 1; i < 8 && j >= 0; i++, j--)
+			{
+				if (board[i, j])
+				{
+					result.Add(new List<int>() { i, j });
+					break;
+				}
+			}
+
+			// diagonal bottom right
+			for (int i = x + 1, j = y + 1; i < 8 && j < 8; i++, j++)
+			{
+				if (board[i, j])
+				{
+					result.Add(new List<int>() { i, j });
+					break;
+				}
+			}
+			return result;
+
+		}
+	}
+}
---
+++ LeetCode_test/QueensThatCanAttackTheKing_1222_UnitTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class QueensThatCanAttackTheKing_1222_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            //[[0,0],[1,1],[2,2],[3,4],[3,5],[4,4],[4,5]]
+            var Input_Queens = new int[][]
+            {
+                new int[]{0,0},
+                new int[]{1,1},
+                new int[]{2,2},
+                new int[]{3,4},
+                new int[]{3,5},
+                new int[]{4,4},
+                new int[]{4,5}
+            };
+            var Input_king = new int[] { 3, 3 };
+            //[[2,2],[3,4],[4,4]]
+            var Expected = new int[][]
+            {
+                new int[]{2,2},
+                new int[]{3,4},
+                new int[]{4,4}
+            };
+            var Actual = QueensThatCanAttackTheKing_1222.QueensAttacktheKing(Input_Queens, Input_king);
+            Assert.AreEqual(Expected.Length, Actual.Count);
+            foreach (var q in Actual)
+            {
+                Assert.IsTrue(Expected.Any(x=>x.SequenceEqual(q)));
+                
+            }
+        }
+    }
+}
