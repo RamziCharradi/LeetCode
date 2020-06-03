7e377c86c44bdd8fd2316eaec008f54738cc2943
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-06-03 21:14:32 +0200

    1409. Queries on a Permutation With Key - Solution

---
+++ LeetCode/QueriesOnAPermutationWithKey_1409.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class QueriesOnAPermutationWithKey_1409
+    {
+        public static int[] ProcessQueries(int[] queries, int m)
+        {
+            List<int> l = Enumerable.Range(1, m).ToList();
+            Queue<int> Result = new Queue<int>();
+            foreach (var q in queries)
+            {
+                Result.Enqueue(l.IndexOf(q));
+                l.RemoveAt(l.IndexOf(q));
+                l.Insert(0, q);
+            }
+
+            return Result.ToArray();
+        }
+    }
+}
---
+++ LeetCode_test/QueriesOnAPermutationWithKey_1409_UnitTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class QueriesOnAPermutationWithKey_1409_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input_queries = new int[] { 3, 1, 2, 1 };
+            var Input_m = 5;
+            var Expected = new int[] { 2, 1, 2, 1 };
+            var Actual = QueriesOnAPermutationWithKey_1409.ProcessQueries(Input_queries, Input_m);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input_queries = new int[] { 4, 1, 2, 2 };
+            var Input_m = 4;
+            var Expected = new int[] { 3, 1, 2, 0 };
+            var Actual = QueriesOnAPermutationWithKey_1409.ProcessQueries(Input_queries, Input_m);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input_queries = new int[] { 7, 5, 5, 8, 3 };
+            var Input_m = 8;
+            var Expected = new int[] { 6, 5, 0, 7, 5 };
+            var Actual = QueriesOnAPermutationWithKey_1409.ProcessQueries(Input_queries, Input_m);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+    }
+}
