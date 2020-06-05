ff62291c87e4644112eb068a8f7e935092da9138
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-06-05 22:59:40 +0200

    442. Find All Duplicates in an Array - Solution

---
+++ LeetCode/FindAllDuplicatesInAnArray_442.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class FindAllDuplicatesInAnArray_442
+    {
+        public static IList<int> FindDuplicates(int[] nums)
+        {
+            IList<int> res = new List<int>();
+            HashSet<int> hs = new HashSet<int>();
+            foreach (var n in nums)
+            {
+                if (!hs.Add(n))
+                {
+                    res.Add(n);
+                }
+            }
+            return res.ToArray();
+           // return nums.GroupBy(x => x).Where(x => x.Count() == 2).Select(x => x.Key).ToArray();
+
+        }
+    }
+}
---
+++ LeetCode_test/FindAllDuplicatesInAnArray_442_UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class FindAllDuplicatesInAnArray_442_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+            IList<int> Expected = new int[] { 2, 3 };
+            var Actual = FindAllDuplicatesInAnArray_442.FindDuplicates(Input);
+            CollectionAssert.AreEqual(Expected.ToArray(), Actual.ToArray());
+        }
+    }
+}
