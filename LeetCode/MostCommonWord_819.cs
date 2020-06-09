368c17e4b59c905bc56457ac7cffb3ce7952b2ac
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-06-09 18:51:24 +0200

    819. Most Common Word - Solution

---
+++ LeetCode/MostCommonWord_819.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class MostCommonWord_819
+    {
+        public static string MostCommonWord(string paragraph, string[] banned)
+        {
+            return paragraph.Split('!', '?', ',', '.', ';', ' ', '\'')
+                            .Where(x => !string.IsNullOrWhiteSpace(x))
+                            .Select(x => x.ToLower())
+                            .Where(x => !banned.Contains(x))
+                            .GroupBy(x => x)
+                            .ToDictionary(x => x.Key, x => x.Count())
+                            .OrderBy(x => x.Value)
+                            .Last()
+                            .Key;
+
+        }
+    }
+}
---
+++ LeetCode_test/MostCommonWord_819_UNitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class MostCommonWord_819_UNitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input_paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+            var Input_Banned = new string[] { "hit" };
+            var Expected = "ball";
+            var Actual = MostCommonWord_819.MostCommonWord(Input_paragraph, Input_Banned);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+       
+    }
+}
