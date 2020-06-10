3b167c1beea974f6f05a73465ce9545d5cc0fede
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-06-11 00:59:31 +0200

    476. Number Complement - Solution
    C# and c++

---
+++ LeetCode/NumberComplement_476.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class NumberComplement_476
+    {
+        public static int FindComplement(int num)
+        {
+
+            int i = 0;
+            for (; i < 32; i++) if((num >> i) == 1) break;
+            int result = (1 << (i + 1)) - 1;
+            return num ^ result;
+
+             /* c++ - Solution 
+              
+                int i = 0;
+                for (; i < 32; i++) if((num >> i) == 1) break;
+        
+                uint32_t result;
+                result = (UINT32_C(1) << (i + 1)) - 1;
+            
+                return num ^ result;
+
+            */
+
+        }
+    }
+}
---
+++ LeetCode_test/NumberComplement_476_UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class NumberComplement_476_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = 5;
+            var Expected = 2;
+            var Actual = NumberComplement_476.FindComplement(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
