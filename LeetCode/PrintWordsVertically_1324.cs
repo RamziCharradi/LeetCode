fd12e26ddb9db115e77f656dc100598ac5e60fa6
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-06-11 22:43:32 +0200

    1324 - Print Words Vertically - Solution

---
+++ LeetCode/PrintWordsVertically_1324.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class PrintWordsVertically_1324
+    {
+        public static IList<string> PrintVertically(string s)
+        {
+
+            var whitespace = ' ';
+            var words = s.Split(whitespace, StringSplitOptions.RemoveEmptyEntries);
+
+            var max = words.Select(x => x.Length).Max();
+            return Enumerable.Range(0, max)
+                              .Select(i => string.Join( string.Empty,
+                                // If the word is shorter than max, fill with whitespace
+                                words.Select(x => i < x.Length ? x[i] : whitespace))
+                                // No trailing whitespace	
+                                .TrimEnd())
+                                .ToList();
+
+            //var w = s.Split(" ");
+            //var max = w.Select(x => x.Length).Max();
+            //string[] Result = new string[max];
+            //var counter = 0;
+            //for (int j = 0; j < max; j++)
+            //{
+            //    for (int i = 0; i < w.Length; i++)
+            //    {
+            //        if (w[i].Length > j ) 
+            //        {
+            //            if (Result[counter] == "")
+            //            {
+            //                if (!char.IsWhiteSpace(w[i][j])) Result[counter] = w[i][j].ToString();
+            //                else Result[counter] = " ";
+            //            }
+            //            else 
+            //            {
+            //                if (!char.IsWhiteSpace(w[i][j])) Result[counter] += w[i][j].ToString();
+            //                else Result[counter] += " ";
+            //            }
+            //        }
+            //        else
+            //        {
+            //            if (Result[counter] == "") Result[counter] = " ";
+            //            else Result[counter] += " ";
+            //        } 
+
+            //    }
+
+            //    counter++;
+            //}
+
+            //return Result.Select(x=>x.TrimEnd()).ToArray();
+        }
+    }
+}
---
+++ LeetCode_test/PrintWordsVertically_1324_UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class PrintWordsVertically_1324_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = "HOW ARE YOU";
+            IList<string> Expected = new string[] { "HAY", "ORO", "WEU" };
+            var Actual = PrintWordsVertically_1324.PrintVertically(Input);
+            CollectionAssert.AreEqual(Expected.ToArray(), Actual.ToArray());
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input = "TO BE OR NOT TO BE";
+            IList<string> Expected = new string[] { "TBONTB", "OEROOE", "   T" };
+            var Actual = PrintWordsVertically_1324.PrintVertically(Input);
+            CollectionAssert.AreEqual(Expected.ToArray(), Actual.ToArray());
+        }
+    }
+}
