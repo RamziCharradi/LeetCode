a4526f7207bb8684439967a4d675e2fc8f616e04
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-06-24 00:31:31 +0200

    Final Prices With A Special Discount In A Shop - 1475

---
+++ LeetCode/FinalPricesWithASpecialDiscountInAShop_1475.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class FinalPricesWithASpecialDiscountInAShop_1475
+    {
+        public static int[] FinalPrices(int[] prices)
+        {
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    if (prices[j] <= prices[i])
+                    {
+                        prices[i] -= prices[j];
+                        break;
+                    }
+                }
+              
+            }
+            return prices;
+
+        }
+    }
+}
---
+++ LeetCode_test/FinalPricesWithASpecialDiscountInAShop_1475_UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class FinalPricesWithASpecialDiscountInAShop_1475_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = new int[] { 8, 4, 6, 2, 3 };
+            var Expected = new int[] { 4, 2, 4, 2, 3 };
+            var Actual = FinalPricesWithASpecialDiscountInAShop_1475.FinalPrices(Input);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod2()
+        {
+            var Input = new int[] { 1, 2, 3, 4, 5 };
+            var Expected = new int[] { 1, 2, 3, 4, 5 };
+            var Actual = FinalPricesWithASpecialDiscountInAShop_1475.FinalPrices(Input);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod3()
+        {
+            var Input = new int[] { 10, 1, 1, 6 };
+            var Expected = new int[] { 9, 0, 1, 6 };
+            var Actual = FinalPricesWithASpecialDiscountInAShop_1475.FinalPrices(Input);
+            CollectionAssert.AreEqual(Expected, Actual);
+        }
+    }
+}
