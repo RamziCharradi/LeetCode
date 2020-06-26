c9352e61c7d03bdff42fa00e8f6b09286616b8c9
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-06-26 23:08:02 +0200

    1337. The K Weakest Rows in a Matrix

---
+++ LeetCode/TheKWeakestRowsInAMatrix_1337.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class TheKWeakestRowsInAMatrix_1337
+    {
+        public static int[] KWeakestRows(int[][] mat, int k)
+        {
+            return Enumerable.Range(0, mat.Length)
+                             .OrderBy(x => mat[x].Sum()).Take(k).ToArray();
+        }
+    }
+}
---
+++ LeetCode_test/TheKWeakestRowsInAMatrix_1337_UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class TheKWeakestRowsInAMatrix_1337_UnitTest
+    {
+        [TestMethod, Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input_mat = new int[][]
+            {
+                new int[]{1, 1, 0, 0, 0},
+                new int[]{1, 1, 1, 1, 0},
+                new int[]{1, 0, 0, 0, 0},
+                new int[]{1, 1, 0, 0, 0},
+                new int[]{1, 1, 1, 1, 1}
+            };
+            var Input_k = 3;
+            var Expected = new int[] { 2, 0, 3 };
+            var Actual = TheKWeakestRowsInAMatrix_1337.KWeakestRows(Input_mat, Input_k);
+            CollectionAssert.AreEqual(Expected, Actual);
+
+        }
+
+    }
+}
