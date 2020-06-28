270eeb1ac6921f61c7f79cfae24ac39b99e7491a
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-06-28 12:36:51 +0200

    1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ LeetCode/AverageSalaryExcludingTheMinimumAndMaximumSalary_1491.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class AverageSalaryExcludingTheMinimumAndMaximumSalary_1491
+    {
+        public static double Average(int[] salary)
+        {
+            return (double)(salary.Sum() - salary.Max() - salary.Min()) / (salary.Length - 2);
+        }
+    }
+}
