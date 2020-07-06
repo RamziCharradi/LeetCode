9a5b84acb96d230606a7ff4b93df6f5f5b4281e2
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-07-06 11:05:56 +0200

    1502. Can Make Arithmetic Progression From Sequence - c# & c++ solution

---
+++ LeetCode/ArithmeticProgressionFromSequence_1502.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ArithmeticProgressionFromSequence_1502
+    {
+        public static bool CanMakeArithmeticProgression(int[] arr)
+        {
+            Array.Sort(arr);
+            int diff = arr[1] - arr[0];
+            var ans = true;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if(arr[i] - arr[i-1] != diff)
+                {
+                    ans = false;
+                    break;
+                }
+            }
+            return ans;
+
+            /**** c++ *****
+             
+            sort(arr.begin(), arr.end());
+            int diff = arr[1] - arr[0];
+            bool ans = true;
+            for (int i = 1; i < arr.size(); i++)
+            {
+                if (arr[i] - arr[i - 1] != diff)
+                {
+                    ans = false;
+                    break;
+                }
+            }
+            return ans;
+            */
+
+        }
+    }
+}
---
+++ LeetCode_test/ArithmeticProgressionFromSequence_1502_UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class ArithmeticProgressionFromSequence_1502_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void testMethode1()
+        {
+            var Input = new int[] { 3, 5, 1 };
+            var Expected = true;
+            var Actual = ArithmeticProgressionFromSequence_1502.CanMakeArithmeticProgression(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod, Timeout(2_000)]
+        public void testMethode2()
+        {
+            var Input = new int[] { 1, 2, 4 };
+            var Expected = false;
+            var Actual = ArithmeticProgressionFromSequence_1502.CanMakeArithmeticProgression(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
