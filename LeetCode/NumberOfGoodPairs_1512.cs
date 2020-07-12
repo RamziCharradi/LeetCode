49d5ba58b2353c5c0ecbdecd58e4ae2e9ce106c2
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-07-12 23:15:20 +0200

    1512. Number of Good Pairs

---
+++ LeetCode/NumberOfGoodPairs_1512.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class NumberOfGoodPairs_1512
+    {
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            int ans = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j]) ans++;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
