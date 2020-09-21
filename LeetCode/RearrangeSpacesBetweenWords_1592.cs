e028189a866c7c92652db5d40e09f845021f88d3
RamziCharradi <58273799+RamziCharradi@users.noreply.github.com>
2020-09-21 02:43:16 +0200

    1592

---
+++ LeetCode/RearrangeSpacesBetweenWords_1592.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class RearrangeSpacesBetweenWords_1592
+    {
+        public static string ReorderSpaces(string text)
+        {
+            var spaces = text.Count(Char.IsWhiteSpace);
+            var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int gap = words.Count() == 1 ? 0 : spaces / (words.Count() - 1);
+
+            return string.Join(new String(' ', gap), words) + new String(' ', spaces - gap * (words.Count() - 1));
+        }
+    }
+}
---
+++ LeetCode_test/RearrangeSpacesBetweenWords_1592_UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_test
+{
+    [TestClass]
+    public class RearrangeSpacesBetweenWords_1592_UnitTest
+    {
+        [TestMethod,Timeout(2_000)]
+        public void TestMethod1()
+        {
+            var Input = "  this   is  a sentence ";
+            var Expected = "this   is   a   sentence";
+            var Actual = RearrangeSpacesBetweenWords_1592.ReorderSpaces(Input);
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
