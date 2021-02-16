f324b027cd95e28f1f89ee26780db7186cbd6d34
Ramzi Charradi <ramzi_charradi@yahoo.fr>
2021-02-16 01:05:30 +0100

    SumOfUniqueElements_1748
    Simple Linq Solution using  groupby

---
+++ LeetCode/SumOfUniqueElements_1748.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class SumOfUniqueElements_1748
+    {
+        public int SumOfUnique(int[] nums)
+        {
+
+            return nums.GroupBy(x => x)
+                       .Where(x => x.Count() < 2)
+                       .Select(x => x.Key)
+                       .Sum();
+
+        }
+
+    }
+}
